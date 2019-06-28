de56d1502b35841f10240b7d7234b83dc1a433f8
xedre <git@xed.re>
2019-06-28 21:20:22 +0100

    updated batch file to setup virtual environment
    Signed-off-by: xedre <git@xed.re>

--- mysterious_moose/Anthropodemic.bat
+++ mysterious_moose/Anthropodemic.bat
@@ -1,2 +1,9 @@
-
-start venv/Scripts/python -m src
+if not exist venv/ (
+    python -m venv venv
+    venv\Scripts\activate.bat
+    python -m pip install -r requirements.txt
+    python -m src
+) else (
+    venv\Scripts\activate.bat
+    python -m src
+)
