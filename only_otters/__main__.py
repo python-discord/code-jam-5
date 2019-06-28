564339bae2fc6cacbb92212e8957a1fa00fe8519
ravenkls <kristian.smith.2001@hotmail.co.uk>
2019-06-28 20:53:23 +0100

    basic skeleton

---
+++ only_otters/__main__.py
@@ -0,0 +1,10 @@
+from .app import MusicPlayer
+from PyQt5 import QtWidgets
+import sys
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    player = MusicPlayer()
+    player.show()
+    sys.exit(app.exec_())
---
+++ only_otters/app.py
@@ -0,0 +1,13 @@
+from PyQt5 import QtWidgets, QtCore
+
+
+class MusicPlayer(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Music Player')
+        self.resize(1280, 720)
+
+        label = QtWidgets.QLabel('Hello World')
+        label.setAlignment(QtCore.Qt.AlignCenter)
+
+        self.setCentralWidget(label)
