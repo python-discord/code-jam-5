05b7a2a8ed304f9b992ece48c4ae510a0f2909b3
scOwez <owez@scalist.net>
2019-06-28 13:40:22 +0100

    All basic python modules Basic click functionality

---
+++ practical_porcupines/__main__.py
@@ -0,0 +1,4 @@
+from cli import base_group
+
+if __name__ == "__main__":
+    base_group()
---
+++ practical_porcupines/cli.py
@@ -0,0 +1,38 @@
+import click
+
+
+@click.group()
+def base_group():
+    pass
+
+
+@click.command()
+def flask_webportal():
+    """
+    Runs the webportal mini-project (like the discord bot but on a website ui)
+    """
+
+    pass
+
+
+@click.command()
+def discord_bot():
+    """
+    Runs the discord bot mini-project, displaying all from the flask_api mini-project
+    """
+
+    pass
+
+
+@click.command()
+def flask_api():
+    """
+    The core API of this project, RESTFUL and built with flask & flask-restful
+    """
+
+    pass
+
+
+base_group.add_command(flask_api)
+base_group.add_command(discord_bot)
+base_group.add_command(flask_webportal)
