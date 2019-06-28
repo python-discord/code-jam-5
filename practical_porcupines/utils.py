fe3e7fc4c937e336bdae1b95f0107dd8551d84f5
scOwez <owez@scalist.net>
2019-06-28 14:05:07 +0100

    Added a lot more to Api and Bot in utils.py Added docstrings to utils.py

--- practical_porcupines/utils.py
+++ practical_porcupines/utils.py
@@ -1,12 +1,40 @@
+import os
 import json
 
 class Config:
+    """
+    Contains all crutial infomation for this project
+    """
+
     class Base:
+        """
+        Basic need-to-know info for all mini-projects
+        """
+
         CONFIG_PATH = "config.json"
         CONFIG = json.load(open(CONFIG_PATH, "r"))
 
     class Api:
-        base_config = Base()
+        """
+        Api endpoints to connect to and host from
+        """
+
+        base_config = Config.Base()
 
         API_DOMAIN = base_config.CONFIG["api"]["domain"]
-        API_PORT = base_config.CONFIG["api"]["port"]
+        API_PORT = base_config.CONFIG["api"]["port"] # NOTE cast to int with flask_api mini-project
+
+    class Bot:
+        """
+        Infomation like the client token for discord_bot mini-project
+        """
+
+        TOKEN = _get_bot_token()
+
+        def _get_bot_token():
+            """
+            Returns client token from `CLIENT_TOKEN` env var
+            """
+
+            return os.environ["CLIENT_TOKEN"]
+    
