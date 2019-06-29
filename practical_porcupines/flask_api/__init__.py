d0ac5cdcbb051676b1e7729d56be16138ce1f0da
scOwez <owez@scalist.net>
2019-06-29 17:06:36 +0100

    Oops, fixed difference calc

--- practical_porcupines/flask_api/__init__.py
+++ practical_porcupines/flask_api/__init__.py
@@ -1,7 +1,7 @@
 from flask_api.app import flask_api_app
 
 # Testing difference calc
-from difference_calc import WLDifference
+from flask_api.difference_calc import WLDifference
 wl_dif = WLDifference()
 print(wl_dif.calculate("29:06:2010 17:02:39", "29:06:2019 17:02:39"))
 # End testing
