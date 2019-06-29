83bf4bd5205f33c55e51d2cfff06bd94ffce7f26
scOwez <owez@scalist.net>
2019-06-29 13:53:32 +0100

    Added the secret key into app.py

--- YOUR_TEAM_NAME/flask_api/app.py
+++ YOUR_TEAM_NAME/flask_api/app.py
@@ -1,10 +1,11 @@
+from utils import ConfigApi
 from flask import Flask
 # from flask_restful import x # TODO add flask-restful
 from flask_sqlalchemy import SQLAlchemy
 
 flask_api_app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "abc"
+app.config["SECRET_KEY"] = ConfigApi().SECRET_KEY
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///waterlevel.sqlite3"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
