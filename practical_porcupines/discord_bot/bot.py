c2ee28ab8312f9ad3cf365dcce0536ba4993440c
scOwez <owez@scalist.net>
2019-06-29 20:07:19 +0100

    Added basic bot

--- practical_porcupines/discord_bot/bot.py
+++ practical_porcupines/discord_bot/bot.py
@@ -6,5 +6,13 @@ config_api = ConfigApi()
 config_bot = ConfigBot()
 
 bot_client = commands.Bot(command_prefix=config_bot.PREFIX)
-
 API_ENDPOINT = f"{config_api.API_DOMAIN}:{config_api.API_PORT}"
+
+
+@client.event
+async def on_ready():
+    """
+    Runs when client boots
+    """
+
+    print(f"{client.user.name} is online with the id of '{client.user.id}'!")
