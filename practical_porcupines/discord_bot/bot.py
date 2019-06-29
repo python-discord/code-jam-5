cf3a02b22ed122dcf252ee9c2282a77f0e937153
scOwez <owez@scalist.net>
2019-06-29 20:06:14 +0100

    Hooked up prefix to right object..

--- practical_porcupines/discord_bot/bot.py
+++ practical_porcupines/discord_bot/bot.py
@@ -1,8 +1,10 @@
 import discord
 from discord.ext import commands
-from practical_porcupines.utils import ConfigApi
+from practical_porcupines.utils import ConfigApi, ConfigBot
 
 config_api = ConfigApi()
-bot_client = commands.Bot(command_prefix=config_api.PREFIX)
+config_bot = ConfigBot()
+
+bot_client = commands.Bot(command_prefix=config_bot.PREFIX)
 
 API_ENDPOINT = f"{config_api.API_DOMAIN}:{config_api.API_PORT}"
