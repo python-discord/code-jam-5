74464b3aa2cd1d9917bea3ebcf1f282e11758312
xedre <git@xed.re>
2019-06-29 11:48:00 +0100

    tests to see if mouse has pressed a button
    with added colour!
    
    Signed-off-by: xedre <git@xed.re>

--- mysterious_moose/src/menus.py
+++ mysterious_moose/src/menus.py
@@ -12,7 +12,16 @@ class MainMenu:
         ],
             []  # list edited by self.resolution_change()
         ]
+        self.intersects = dict()
         self.resolution_change()
+
+        # button colours
+        self.button_colour = (200, 100, 50)
+        self.button_hover_colour = (100, 100, 50)
+        self.button_click_colour = (100, 0, 50)
+
+        # True if the mouse is currently pressed
+        self.pressed = False
 
     def resolution_change(self):
         # get the display size
@@ -47,7 +56,7 @@ class MainMenu:
             text="Play",
             size=40
         )[0]
-        play_text_loc = play_text.get_rect(center=(resolution[0]/2, title_size[1] + 65))
+        play_text_pos = play_text.get_rect(center=(resolution[0]/2, title_size[1] + 65))
 
         # options
         options_button = pygame.Rect(
@@ -60,24 +69,51 @@ class MainMenu:
             text="Options",
             size=40
         )[0]
-        options_text_loc = options_text.get_rect(center=(resolution[0] / 2, title_size[1] + 125))
+        options_text_pos = options_text.get_rect(center=(resolution[0] / 2, title_size[1] + 125))
 
         # puts elements in graphics to be rendered
         self.graphics[1] = [
             {"type": "surface", "surface": title, "dest": (resolution[0]//10, 20)},
-            {"type": "rect", "rect": play_button, "colour": (234, 124, 176)},
-            {"type": "surface", "surface": play_text, "dest": play_text_loc},
-            {"type": "rect", "rect": options_button, "colour": (234, 124, 176)},
-            {"type": "surface", "surface": options_text, "dest": options_text_loc}
+            {"type": "rect", "rect": play_button, "colour": self.button_colour},
+            {"type": "surface", "surface": play_text, "dest": play_text_pos},
+            {"type": "rect", "rect": options_button, "colour": self.button_colour},
+            {"type": "surface", "surface": options_text, "dest": options_text_pos}
         ]
+
+        # dict of intersects for the mouse
+        self.intersects = {"play": play_button, "options": options_button}
 
     def display(self, events):
         # make a copy of graphics for editing
         graphics = self.graphics
 
-        for event in events:
-            if event.type == pygame.MOUSEMOTION:
-                mouse = pygame.Rect(event.pos[0], event.pos[1], 1, 1)
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_pos = pygame.Rect(mouse_pos[0], mouse_pos[1], 1, 1)
+        mouse_state = pygame.mouse.get_pressed()
+
+        if mouse_pos.collidelist(self.intersects):
+            over = mouse_pos.collidedict(self.intersects)[0]
+            if mouse_state[0] is True:
+                self.pressed = True
+                if over == "play":
+                    self.graphics[1][1]["colour"] = self.button_click_colour
+                elif over == "options":
+                    self.graphics[1][1]["colour"] = self.button_click_colour
+            elif mouse_state[0] is False and self.pressed is True:
+                self.pressed = False
+                if over == "play":
+                    return 3
+                elif over == "options":
+                    return 2
+            else:
+                if over == "play":
+                    self.graphics[1][1]["colour"] = self.button_hover_colour
+                if over == "options":
+                    self.graphics[1][1]["colour"] = self.button_hover_colour
+        else:
+            self.graphics[1][1]["colour"] = self.button_colour
+            self.graphics[1][3]["colour"] = self.button_colour
+
         self.renderer.update(graphics)
 
 
