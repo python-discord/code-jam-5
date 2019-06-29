fdfb14453a9c3cd473fedb4dba98c7afbc850adb
Valiant55 <matthew.g.stoltz@gmail.com>
2019-06-29 09:11:09 -0400

    added output for player class

--- daily_dragons/main.py
+++ daily_dragons/main.py
@@ -1,10 +1,14 @@
 from planet import Planet
+from player import Player
 
 
 def main():
     earth = Planet()
     player_name = input("What is your name? ")
+    player = Player(player_name)
+
     print(f"Hello, {player_name}")
+    print(player)
     print("Current Earth Stats:")
     print(earth)
 
