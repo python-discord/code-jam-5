b03718988cb071146c8a2629ab41dca36b63fbee
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-06-29 12:53:17 +0300

    main menu page

--- various_vipers/project/constants.py
+++ various_vipers/project/constants.py
@@ -1,9 +1,15 @@
+"""All game contstants."""
 
 FPS = 60
 
 WIDTH = 1200
-HEIGHT = 800
+HEIGHT = 700
 
 
 class Color:
+    """Represent RGB color value."""
+
     black = (0, 0, 0)
+    white = (255, 255, 255)
+
+    red = (255, 0, 0)
--- various_vipers/project/game.py
+++ various_vipers/project/game.py
@@ -1,31 +1,43 @@
+"""Game model."""
 import pygame as pg
 
-from project.constants import WIDTH, HEIGHT, FPS, Color
+from project.UI.main_menu import MainMenu
+from project.constants import Color, FPS, HEIGHT, WIDTH
 
 
 class Game:
-    """
-    Represents main game class
-    """
+    """Represents main game class."""
+
     def __init__(self):
+        """Set initial values."""
         pg.init()
-        pg.display.set_caption('Various Vipers game in development')
+        pg.display.set_caption("Various Vipers game in development")
 
         self.running = True
+
+        self.mouse_x = self.mouse_y = int()
 
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         self.clock = pg.time.Clock()
 
+        self.main_menu = MainMenu(self.screen)
+
     def run(self):
+        """Draw and get events."""
         self.clock.tick(FPS)
         self._get_events()
         self._draw()
 
     def _get_events(self):
+        self.mouse_x, self.mouse_y = pg.mouse.get_pos()
+
+        print(self.mouse_x, self.mouse_y)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 self.running = False
 
     def _draw(self):
         self.screen.fill(Color.black)
+        self.main_menu.draw()
+
         pg.display.flip()
