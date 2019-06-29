9e735c114994379b9c6bf36be1b52e5bda33003e
scOwez <owez@scalist.net>
2019-06-29 23:01:05 +0100

    Docstring for get_difference in api.py

--- practical_porcupines/discord_bot/api.py
+++ practical_porcupines/discord_bot/api.py
@@ -3,4 +3,14 @@ from practical_porcupines.utils import ConfigApi
 
 config_api = ConfigApi()
 
-API_ENDPOINT = f"{config_api.API_DOMAIN}:{config_api.API_PORT}"
+API_ENDPOINT = f"{config_api.API_DOMAIN}:{config_api.API_PORT}/api"
+
+async def get_difference(time_1, time_2):
+    """
+    > Sends time_1 and time_2 to flask_api
+    - time_1 = Start time (%Y:%m:%d:%T)
+    - time_2 = End time (%Y:%m:%d:%T)
+    < Returns float of mm difference
+    """
+
+    pass
