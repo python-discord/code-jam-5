458c4327823e56fa778e2d835aa781202660355b
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-06-29 14:33:18 +0300

    main menu buttons

---
+++ various_vipers/project/UI/button.py
@@ -0,0 +1,43 @@
+"""Button model."""
+
+import pygame as pg
+from pygame import Rect
+
+from project.constants import HEIGHT, WIDTH
+
+
+def generate_buttons(btn_w: int, btn_h: int, btn_count: int, gap: int) -> list:
+    """Generates new buttons of given parameters."""
+    buttons = list()
+
+    for i in range(btn_count):
+        new_button = ButtonModel(
+            btn_w=btn_w, btn_h=btn_h, btn_count=btn_count, pos=i + 1, gap=gap
+        )
+
+        buttons.append(new_button)
+    return buttons
+
+
+class ButtonModel:
+    """Represents a button."""
+
+    def __init__(self, btn_w: int, btn_h: int, btn_count: int, pos: int, gap: int):
+
+        btn_area = btn_h * btn_count
+        gap_area = (btn_count - 1) * gap
+
+        self.margin_x = (WIDTH - btn_w) / 2
+        self.margin_y = (HEIGHT - (btn_area + gap_area)) / 2
+
+        self.left = self.margin_x
+        self.top = self.margin_y + (gap * (pos - 1)) + (btn_h * (pos - 1))
+
+        self.width = btn_w
+        self.height = btn_h
+
+        self.rect = Rect(self.left, self.top, self.width, self.height)
+
+    def draw(screen: pg.Surface, hover: bool):
+        """Draws the button on screen."""
+        pass
--- various_vipers/project/constants.py
+++ various_vipers/project/constants.py
@@ -3,16 +3,24 @@ import logging
 
 FPS = 60
 
-WIDTH = 1200
-HEIGHT = 700
+WIDTH = 1280
+HEIGHT = 720
 
 LOG_LEVEL = logging.DEBUG
 
 
 class Color:
-    """Represent RGB color value."""
+    """Represent RGB color values."""
 
     black = (0, 0, 0)
     white = (255, 255, 255)
 
     red = (255, 0, 0)
+
+
+class Button:
+    """Represents buttons properties."""
+
+    main_btn_w = 400
+    main_btn_h = 100
+    btn_gap = 50
