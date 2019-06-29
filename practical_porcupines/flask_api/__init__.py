6002f2713e20bfe759275823e721df786375626e
scOwez <owez@scalist.net>
2019-06-29 20:13:16 +0100

    Made the random `None` in buffer make sense

--- practical_porcupines/flask_api/__init__.py
+++ practical_porcupines/flask_api/__init__.py
@@ -4,5 +4,8 @@ from practical_porcupines.flask_api.app import flask_api_app
 from practical_porcupines.flask_api.difference_calc import WLDifference
 
 wl_dif = WLDifference()
-print(wl_dif.calculate("29:06:2010 17:02:39", "29:06:2019 17:02:39"))
+print(
+    "flask_api testing calculation: "
+    + str(wl_dif.calculate("29:06:2010 17:02:39", "29:06:2019 17:02:39"))
+)
 # End testing
