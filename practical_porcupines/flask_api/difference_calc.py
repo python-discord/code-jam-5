d03ee4f19ca1028c650e5dce56a07a9d8ab2aca1
scOwez <owez@scalist.net>
2019-06-29 17:03:21 +0100

    Added testing of calculator to __init__.py in flask_api Basic class

---
+++ practical_porcupines/flask_api/difference_calc.py
@@ -0,0 +1,12 @@
+from models import LevelModel
+
+class WLDifference:
+    def calculate(date_1, date_2):
+        """
+        Calculates difference of global water level between date_1 and date_2
+        Returns the difference in mm as a float.
+
+        NOTE This is a frontend function and should hook to lower-level ones
+        """
+
+        pass
