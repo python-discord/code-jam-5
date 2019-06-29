047c5c2f4db3cb8c336122946af9518b84c48af3
Valiant55 <matthew.g.stoltz@gmail.com>
2019-06-29 13:48:50 -0400

    defined init method, which takes an extra parameter, roi

--- daily_dragons/company.py
+++ daily_dragons/company.py
@@ -1,5 +1,20 @@
 from organization import Organization
+from planetary_effects import PlanetaryEffects
 
 
 class Company(Organization):
-    pass
+    """
+    Company extends Organization
+
+    A company, unlike an NGO, has a return on investiment (roi)
+    that increases the player's net worth when invested into
+
+    """
+
+    def __init__(
+        self, name: str, description: str, planetary_effects: PlanetaryEffects, roi: int
+    ) -> None:
+        """
+        """
+        super().__init__(name, description, planetary_effects)
+        self.roi = 0
