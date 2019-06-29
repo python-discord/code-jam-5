ae49e598a60d93a5e5ca7c85eeae1e3b210de845
scOwez <owez@scalist.net>
2019-06-29 12:23:02 +0100

    Restructured utils.py to make it work Formatted utils.py

--- YOUR_TEAM_NAME/utils.py
+++ YOUR_TEAM_NAME/utils.py
@@ -13,50 +13,49 @@
 import os
 import json
 
-class Config:
+
+class ConfigBase:
     """
-    Contains all crutial infomation for this project
+    Basic need-to-know info for all mini-projects
     """
 
-    class Base:
+    CONFIG_PATH = "config.json"
+    CONFIG = json.load(open(CONFIG_PATH, "r"))
+
+
+class ConfigApi:
+    """
+    Api endpoints to connect to and host from
+    """
+
+    config = ConfigBase().CONFIG
+
+    API_DOMAIN = config["api"]["domain"]
+    API_PORT = config["api"]["port"]
+
+
+class ConfigWebPortal:
+    """
+    WebPortal endpoints to connect to and host from
+    """
+
+    config = ConfigBase().CONFIG
+
+    API_DOMAIN = config["web_portal"]["domain"]
+    API_PORT = config["web_portal"]["port"]
+
+
+class ConfigBot:
+    """
+    Infomation like the client token for discord_bot mini-project
+    """
+
+    def __init__(self):
+        self.TOKEN = self._get_bot_token()
+
+    def _get_bot_token(self):
         """
-        Basic need-to-know info for all mini-projects
+        Returns client token from `CLIENT_TOKEN` env var
         """
 
-        CONFIG_PATH = "config.json"
-        CONFIG = json.load(open(CONFIG_PATH, "r"))
-
-    class Api:
-        """
-        Api endpoints to connect to and host from
-        """
-
-        base_config = Config.Base()
-
-        API_DOMAIN = base_config.CONFIG["api"]["domain"]
-        API_PORT = base_config.CONFIG["api"]["port"]
-
-    class WebPortal:
-        """
-        WebPortal endpoints to connect to and host from
-        """
-
-        base_config = Config.Base()
-
-        API_DOMAIN = base_config.CONFIG["web_portal"]["domain"]
-        API_PORT = base_config.CONFIG["web_portal"]["port"]
-
-    class Bot:
-        """
-        Infomation like the client token for discord_bot mini-project
-        """
-
-        TOKEN = _get_bot_token()
-
-        def _get_bot_token():
-            """
-            Returns client token from `CLIENT_TOKEN` env var
-            """
-
-            return os.environ["CLIENT_TOKEN"]
-    
+        return os.environ["CLIENT_TOKEN"]
