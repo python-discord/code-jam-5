e292fc0217b889593ad8fb80f74c9cf4f4a9a824
700y <700y@users.noreply.github.com>
2019-06-29 15:47:18 +0100

    Calculated x and y from coodinates

--- hushed_hyenas/game.py
+++ hushed_hyenas/game.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import pygame
 import utm
 
@@ -5,6 +7,7 @@ pygame.init()
 
 
 class Game:
+
     def __init__(self):
         self.width = 1200
         self.height = 600
@@ -13,30 +16,37 @@ class Game:
         self.window = pygame.display.set_mode((self.width, self.height))
         self.caption = pygame.display.set_caption('Code jam')
         self.map = pygame.image.load(r'map_objects/earth2.png')
-        self.map = pygame.transform.scale(self.map, (self.width, self.height))  # Resize image to fit in window
+        self.map = pygame.transform.scale(self.map, (self.width,
+                self.height))  # Resize image to fit in window
 
         # Example latitude and longitude from Australia
+
         self.lat = -25.274398
         self.lon = 133.775136
 
         # Transform lat and lon into x and y coordinates with UTM
+
         self.x_utm = utm.from_latlon(self.lat, self.lon)
         self.y_utm = utm.from_latlon(self.lat, self.lon)
+
+        # Transforming directly to x and y
+
+        self.x = (180 + self.lon) / 360 * self.width
+        self.y = (90 + self.lat * -1) / 180 * self.height
 
     def run(self):
         run = True
         window = self.window
         clock = pygame.time.Clock()
 
-        # Printing for debugging to see if it's being correctly converted
-        print(self.x_utm)
-        print(self.y_utm)
-
         # Need to transform utm to x and y
+
         while run:
             clock.tick(60)  # Set to 60 fps
             window.fill(self.bg_color)
             window.blit(self.map, (0, 0))
+            pygame.draw.circle(window, (0, 0, 0), (int(self.x),
+                               int(self.y)), 5)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
