ce984df958b8b6a9ce7f591ed0d4d53803355c16
Valiant55 <matthew.g.stoltz@gmail.com>
2019-06-29 13:49:47 -0400

    added string representation

--- daily_dragons/planetary_effects.py
+++ daily_dragons/planetary_effects.py
@@ -2,6 +2,12 @@ from typing import Dict
 
 
 class PlanetaryEffects:
+    """
+    PlanetaryEffects
+
+    Helper class for changing the affects player decisions have on the planet
+    """
+
     def __init__(
         self, bio_diversity: int, temperature: int, co2: int, habitable_land: int
     ) -> None:
@@ -9,3 +15,25 @@ class PlanetaryEffects:
         self.temperature = temperature
         self.co2 = co2
         self.habitable_land = habitable_land
+
+    def __sign_helper(self, stat: int) -> str:
+        if stat >= 0:
+            return "+"
+        else:
+            return "-"
+
+    def __str__(self) -> str:
+
+        bio = __sign_helper(self.bio_diversity) + str(self.bio_diversity)
+        temp = __sign_helper(self.temperature) + str(self.temperature)
+        carbon = __sign_helper(self.co2) + str(self.co2)
+        land = __sign_helper(self.habitable_land) + str(self.habitable_land)
+
+        net_effects = (
+            f"Biodiversity: {bio}\n"
+            f"Temperature: {temp}\n"
+            f"CO2: {carbon}\n"
+            f"Habitable Land: {land}\n"
+        )
+
+        return net_effects
