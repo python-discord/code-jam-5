60824ac02f89171e0b072553bf15fbec3aa1714b
Adam <PseudoDesign@users.noreply.github.com>
2019-06-29 02:30:52 -0600

    Removed old tests

--- ultrasonic_unicorns/tests/test_environment.py
+++ ultrasonic_unicorns/tests/test_environment.py
@@ -4,6 +4,3 @@ from unittest import TestCase
 class TestAlwaysPass(TestCase):
     def test_true_is_true(self):
         self.assertTrue(True)
-
-    def test_false_is_false(self):
-        self.assertFalse(False)
