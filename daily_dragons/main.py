87d316b1a2b04b84fef332b08121f86948f9290b
Valiant55 <matthew.g.stoltz@gmail.com>
2019-06-29 09:03:35 -0400

    added output of the current planet's stats

--- daily_dragons/main.py
+++ daily_dragons/main.py
@@ -1,6 +1,12 @@
+from planet import Planet
+
+
 def main():
+    earth = Planet()
     player_name = input("What is your name? ")
     print(f"Hello, {player_name}")
+    print("Current Earth Stats:")
+    print(earth)
 
 
 if __name__ == "__main__":
