1bb5035d54c3ab82e6710f3d8faab34b504e2c11
scOwez <owez@scalist.net>
2019-06-29 15:54:00 +0100

    Added 404 page

--- practical_porcupines/flask_webportal/app.py
+++ practical_porcupines/flask_webportal/app.py
@@ -5,3 +5,7 @@ flask_webportal_app = Flask(__name__)
 @flask_webportal_app.route("/")
 def index():
     return render_template("index.html")
+
+@flask_webportal_app.errorhandler(404)
+def error_404(e):
+    return render_template("error_404.html")
