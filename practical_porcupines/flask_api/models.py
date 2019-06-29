3aac928570fedf6a4da904fb2e9e6235f96c1de8
scOwez <owez@scalist.net>
2019-06-29 19:11:28 +0100

    Basic primary key in models.py

--- practical_porcupines/flask_api/models.py
+++ practical_porcupines/flask_api/models.py
@@ -5,4 +5,10 @@ class LevelModel(db.Model):
     Contains mean values from each year & the corrosponding global water level
     """
 
-    pass
+    __tablename__ = "levels"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __init__(self, id):
+        self.id = id
+    
