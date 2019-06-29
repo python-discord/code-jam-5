e1dcd84cf07e6994d18b4e5db30ed27a98f342c7
TizzySaurus <tizzysaurus@gmail.com>
2019-06-29 20:46:22 +0100

    Fixed errors in __main__.py

--- only_otters/__main__.py
+++ only_otters/__main__.py
@@ -1,9 +1,9 @@
-from .app import MusicPlayer
+from app import MusicPlayer
 from PyQt5 import QtWidgets
 import sys
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     gui = MusicPlayer()
     sys.exit(app.exec_())
