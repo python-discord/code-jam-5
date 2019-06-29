4ead4b6f8e8b55a30c5b7cd7cc80d0cda4d0b5d8
scOwez <owez@scalist.net>
2019-06-29 19:55:42 +0100

    Added about page Linked up about to index Linked up about to navbar

--- practical_porcupines/flask_webportal/app.py
+++ practical_porcupines/flask_webportal/app.py
@@ -8,6 +8,11 @@ def index():
     return render_template("index.html")
 
 
+@flask_webportal_app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @flask_webportal_app.errorhandler(404)
 def error_404(e):
     return render_template("error_404.html")
