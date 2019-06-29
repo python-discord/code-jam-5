cbecdb89500a55621515fed31e087ec186c29137
TizzySaurus <tizzysaurus@gmail.com>
2019-06-29 21:08:19 +0100

    Added docstrings and made flake8 compliant

--- only_otters/app.py
+++ only_otters/app.py
@@ -1,10 +1,13 @@
-import sys
 from PyQt5.QtCore import QUrl
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaPlaylist, QMediaContent
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QWidget, QPushButton, QVBoxLayout, QHBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QWidget, QPushButton, QVBoxLayout, QHBoxLayout
 
 
 class MusicPlayer(QMainWindow):
+    """
+    Initializes a Qt Window with an embedded media player.
+    Add control buttons, and ask the user to select two audio files.
+    """
     def __init__(self):
         super().__init__()
         self.title = "Only Otters"
@@ -12,44 +15,45 @@ class MusicPlayer(QMainWindow):
         self.playlist = QMediaPlaylist()
         self.createUI()
         self.addButtons()
-        self.openFile() #select audio file to play
-        self.openFile() #select second audio file to play (stored in playlist)
+        self.openFile()  # select audio file to play
+        self.openFile()  # select second audio file to play (stored in playlist)
 
     def createUI(self):
-        menubar = self.menuBar()
+        """Create basic shape of the GUI."""
         self.setWindowTitle(self.title)
         self.setGeometry(250, 250, 250, 200)
         self.show()
 
     def addButtons(self):
+        """Create buttons of the GUI."""
         widget = QWidget(self)
-        
+
         self.setCentralWidget(widget)
-        #song controls
+        # song controls
         playButton = QPushButton("Play")
         pauseButton = QPushButton("Pause")
         stopButton = QPushButton("Stop")
-        #playlist controls
+        # playlist controls
         shuffleButton = QPushButton("Shuffle")
         prevButton = QPushButton("Prev")
         nextButton = QPushButton("Next")
-        #button layouts
+        # button layouts
         centralWidget = QVBoxLayout()
         controls = QHBoxLayout()
         playlistCtrlLayout = QHBoxLayout()
-        #layout of buttons for song control
+        # layout of buttons for song control
         controls.addWidget(playButton)
         controls.addWidget(pauseButton)
         controls.addWidget(stopButton)
-        #layout of buttons for playlist control
+        # layout of buttons for playlist control
         playlistCtrlLayout.addWidget(shuffleButton)
         playlistCtrlLayout.addWidget(prevButton)
         playlistCtrlLayout.addWidget(nextButton)
-        #vertical layout
+        # vertical layout
         centralWidget.addLayout(controls)
         centralWidget.addLayout(playlistCtrlLayout)
         widget.setLayout(centralWidget)
-        #assign button to corresponding function
+        # assign button to corresponding function
         playButton.clicked.connect(self.play)
         pauseButton.clicked.connect(self.pause)
         stopButton.clicked.connect(self.stop)
@@ -57,53 +61,59 @@ class MusicPlayer(QMainWindow):
         shuffleButton.clicked.connect(self.shuffle)
         prevButton.clicked.connect(self.prevSong)
         nextButton.clicked.connect(self.nextSong)
-        
+
         self.statusBar()
         self.playlist.currentMediaChanged.connect(self.songChanged)
 
     def play(self):
-        #if no songs in queue select a file else play 1st song from playlist
+        """If no songs in queue select a file else play 1st song from playlist."""
         if not self.playlist.mediaCount():
             self.openFile()
         else:
             self.player.play()
-    
+
     def pause(self):
+        """Pause the song."""
         self.player.pause()
 
     def stop(self):
+        """Stop song and clear playlist."""
         self.player.stop()
         self.playlist.clear()
         self.statusBar().showMessage("Stopped and emptied playlist")
 
     def shuffle(self):
+        """Shuffle playlist."""
         self.playlist.shuffle()
 
     def prevSong(self):
+        """Play previous song."""
         if not self.playlist.mediaCount():
             self.openFile()
         else:
             self.player.playlist().previous()
 
     def nextSong(self):
+        """Play next song."""
         if not self.playlist.mediaCount():
             self.openFile()
         else:
             self.player.playlist().next()
 
     def songChanged(self, media):
+        """Display currently playing song."""
         if not media.isNull():
             url = media.canonicalUrl()
             self.statusBar().showMessage(f"Playing: {url.fileName().split('.')[0]}")
 
     def openFile(self):
+        """Select music file to play."""
         song = QFileDialog.getOpenFileName(self, "Open Song", "", "Sound Files (*.mp3)")
 
         if song[0] != '':
             url = QUrl.fromLocalFile(song[0])
-            print(song)
-            
-            #if playlist is empty then play song, otherwise add to playlist
+
+            # if playlist is empty then play song, otherwise add to playlist
             if not self.playlist.mediaCount():
                 self.playlist.addMedia(QMediaContent(url))
                 self.player.setPlaylist(self.playlist)
