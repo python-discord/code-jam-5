5304534f17eedde4c11ad89461f921c82ed48c5e
Horiwix <auseviciusss@gmail.com>
2019-06-29 12:29:00 +0300

    Now able to scroll with left+right keys.

--- various_vipers/project/gameplay/game_view.py
+++ various_vipers/project/gameplay/game_view.py
@@ -1,21 +1,26 @@
 import logging
+
 import pygame as pg
+
 
 logger = logging.getLogger(__name__)
 
 
 class GameView:
+    """GameView hold the information about all things related to the main game."""
+
     def __init__(self):
         pass
 
     def update(self):
         """
-        This function is called every game tick.
+        Update is called every game tick.
+
         Function handles basic gameplay elements and inputs.
         """
         key_pressed = pg.key.get_pressed()
 
-        if key_pressed[pg.K_a]:
-            logger.debug("Pressed A key.")
-        if key_pressed[pg.K_d]:
-            logger.debug("Pressed D key.")
+        if key_pressed[pg.K_a] or key_pressed[pg.K_LEFT]:
+            logger.debug("Scrolling LEFT.")
+        if key_pressed[pg.K_d] or key_pressed[pg.K_RIGHT]:
+            logger.debug("Scrolling RIGHT.")
