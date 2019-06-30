e9b116e9a6cb8cb70b76d0cf5c4fb02d40c3b81d
Bast0006 <bast@fastmail.com>
2019-06-30 02:57:52 -0700

    Fix angle_between to function properly when using pyglet

--- aesthetic_ants/penguin/utils.py
+++ aesthetic_ants/penguin/utils.py
@@ -6,6 +6,8 @@ def angle_between(x1: float, y1: float, x2: float, y2: float) -> float:
     """
     Returns the angle between the coordinates (x1, y1) and (x2, y2) in radians
     """
-    dx = x1 - x2
-    dy = y1 - y2
-    return math.atan2(dy, dx)
+    dx = x2 - x1
+    dy = y2 - y1
+
+    # We return negative because pyglet and math treat rotation differently
+    return -math.atan2(dy, dx)
