9cd17af70249d8678935072fe8e389a57eca650c
Valiant55 <matthew.g.stoltz@gmail.com>
2019-06-30 02:21:09 -0400

    added news lists to investments

--- daily_dragons/investment.py
+++ daily_dragons/investment.py
@@ -1,3 +1,5 @@
+from typing import List
+from news import News
 from organization import Organization
 from planetary_effects import PlanetaryEffects
 
@@ -14,8 +16,14 @@ class Investment:
         self,
         organization: Organization,
         planetary_effects: PlanetaryEffects,
+        news_on_apperance: List[News],
+        news_on_investment: List[News],
+        news_on_no_investment: List[News],
         roi: int = 0,
     ) -> None:
         self.organization = organization
         self.planetary_effects = planetary_effects
+        self.news_on_apperance = news_on_apperance
+        self.news_on_investment = news_on_investment
+        self.news_on_no_investment = news_on_no_investment
         self.roi = roi
