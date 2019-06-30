f37def4bcd7b3c326253aa40bb0796626a81391e
Bast0006 <bast@fastmail.com>
2019-06-30 03:06:46 -0700

    Add player and player hooks to window

--- aesthetic_ants/penguin/main.py
+++ aesthetic_ants/penguin/main.py
@@ -1,9 +1,19 @@
 import pyglet
+from player import Player
 
 window = pyglet.window.Window(caption='Penguin Snowball')
 
+player = Player(window.width / 2, window.height / 2)
+
+
+@window.event
+def on_draw():
+    window.clear()
+    player.draw()
+
 
 def main():
+    window.event(player.on_mouse_motion)  # Registers the player-mouse event handler
     pyglet.app.run()
 
 
