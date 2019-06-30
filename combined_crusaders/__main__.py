41b5d63eab6754cc13ef4647b3e79c172b7b893d
Maku <makusu2@gmail.com>
2019-06-30 15:51:18 -0400

    Got rid of bad module-level variables. Decided not to cache sounds and images. Added media file for sound/image loading.

--- combined_crusaders/__main__.py
+++ combined_crusaders/__main__.py
@@ -1,60 +1,21 @@
 # Some of this skeleton was stolen from the aliens pygame example
 import pygame
-import os
-from pygame.locals import KEYDOWN, K_ESCAPE, QUIT, Rect
+from pygame.rect import Rect
+from pygame.constants import KEYDOWN, K_ESCAPE, QUIT, K_SPACE
+import media
 
-script_dir = os.path.split(os.path.abspath(__file__))[0]
 if not pygame.image.get_extended():
     raise SystemExit("Sorry, extended image module required")
 
 
-def load_images():
-    # return dict of str:pygame.image.
-    # This will be used for sprite images, background images,
-    # and all other images.
-    # If this turns out to not make sense with pygame's setup,
-    # like sprites use a different image form,
-    # we can separate this into multiple functions.
-    # Our goal is to be able to greatly simplify image loading,
-    # so that we only need to say
-    # PolarBear(image=our_images["polar_bear"]) instead of
-    # PolarBear(image=pygame.image(Image.open("Images_Path/PolarBear.jpeg"))
-    # or whatever
-    return {"polar_bear": pygame.image.load(
-        os.path.join(script_dir, 'images', "polar_bear.jpeg")).convert()}
-
-
-def load_sounds():
-    # return dict of str:pygame.mixer.Sound
-    # Basically the same as load_images, just with sounds
-    return {"beep": pygame.mixer.Sound(
-        os.path.join(script_dir, 'sounds', "beep.wav"))}
-
-
-pygame.init()
-screenrect = Rect(0, 0, 640, 480)
-fullscreen = False
-winstyle = 0
-bestdepth = pygame.display.mode_ok(screenrect.size, winstyle, 32)
-screen = pygame.display.set_mode(screenrect.size, winstyle, bestdepth)
-images = load_images()
-sounds = load_sounds()
-# Having this at module-level feels icky,
-# but it's better than having at class level or object level.
-# We can change this later once we having a working game
-# OOOO, wait, we can use a cool functools thing I learned about with some
-# load_image and load_sound that lazy loads and caches the result!
-# I'll handle that.
-
-
 class Button(pygame.sprite.Sprite):
-    def __init__(self):
-        self.image = images["polar_bear"]
+    def __init__(self, button_text, click_sound):
         self.rect = self.image.get_rect()
+        self.click_sound = click_sound
 
     def on_click(self):
         """I made this method up; figure out how we detect button clicks"""
-        sounds["beep"].play()
+        self.click_sound.play()
         # I'm just playing the beep as a test that it's working, it won't
         # be in the final version
         pass
@@ -74,12 +35,21 @@ class CC:  # TODO game name goes here
      instead? maybe? idk lol
     """
     def __init__(self):
+        pygame.init()
+
+        screenrect = Rect(0, 0, 640, 480)
+        bestdepth = pygame.display.mode_ok(screenrect.size, 0, 32)
+        pygame.display.set_mode(screenrect.size, 0, bestdepth)
+
         self.exit_requested = False
         self.score = 0
-        self.environment_image = None  # images["environment_neutral"]
         self.button_panel = ButtonPanel()
         self.clock = pygame.time.Clock()
-        self.fullscreen = False
+
+        self.images = media.load_images()
+        self.sounds = media.load_sounds()
+
+        self.environment_image = None  # images["environment_neutral"]
         # If we want to display the score, that shouldn't be too hard. If
         # we want to display the environment only instead, that works too.
         # TODO display environment_image in background
@@ -96,6 +66,9 @@ class CC:  # TODO game name goes here
                 return
 
         keystate = pygame.key.get_pressed()
+        if keystate[K_SPACE]:
+            self.sounds["beep"].play()
+            # Just for testing sounds
         self.clock.tick(40)
         # Or maybe don't? It's a clicker, so having an fps limit might be dumb
 
@@ -107,8 +80,6 @@ class CC:  # TODO game name goes here
 
 
 def main():
-    if pygame.mixer and not pygame.mixer.get_init():
-        raise OSError("Can't play sounds, sounds are required")
     game = CC()
     game.play()
 
