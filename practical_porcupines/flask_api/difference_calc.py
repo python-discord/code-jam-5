85d87df18f6eb1a80f27c11e9c53d58f2b7a6c6a
scOwez <owez@scalist.net>
2019-06-30 17:47:50 +0100

    Added back dataset stuff

--- practical_porcupines/flask_api/difference_calc.py
+++ practical_porcupines/flask_api/difference_calc.py
@@ -1,6 +1,6 @@
 import os
-from practical_porcupines.flask_api.models import LevelModel
 from datetime import datetime, timedelta
+from practical_porcupines.flask_api.models import LevelModel
 
 
 class WLDifference:
@@ -8,8 +8,10 @@ class WLDifference:
         """
         Calculates difference of global water level between date_1 and date_2
         Returns the difference in mm as a float.
+
         NOTE This is a frontend function and should hook to lower-level ones
         """
+
         pass
 
     def _string_to_datetime(self, decimal_date):
@@ -26,3 +28,23 @@ class WLDifference:
         )
 
         return result
+
+    def parse_data(self):
+        """
+        Parses the dataset from the NASA file. returns what we need as a list of tuples
+        """
+        dataset_path = os.path.join(os.path.dirname(__file__), "DATASET_GMSL.txt")
+        with open(dataset_path, "r+") as f:
+            lines = f.readlines()
+            data = list(filter(lambda x: x.find("HDR") == -1, lines))
+            filtered_data = []
+            for d in data:
+                d = d.split()
+                reading_date_fraction = float(d[2])
+                reading_date = WLDifference.decimal_to_datetime(
+                    reading_date_fraction
+                ).date()  # Date of GMSL reading
+                gmsl = float(d[11])  # GMSL value
+                filtered_data.append((reading_date, gmsl))
+
+        return filtered_data
