ec6734b8f290686ccbed201de9d6fb9fe4c27e49
TizzySaurus <tizzysaurus@gmail.com>
2019-06-30 17:40:18 +0100

    Centralised the 'Now Playing'

--- only_otters/music_player/visualiser.py
+++ only_otters/music_player/visualiser.py
@@ -1,8 +1,8 @@
-from PyQt5 import QtWidgets, QtGui
+from PyQt5 import QtWidgets, QtGui, QtCore
 
 
 class VisualiserWidget(QtWidgets.QWidget):
-    """Visualises the music as it plays from a QMediaPlayer"""
+    """Visualises the music as it plays from a QMediaPlayer."""
 
     def __init__(self, player):
         super().__init__()
@@ -17,6 +17,7 @@ class VisualiserWidget(QtWidgets.QWidget):
         now_playing_font = QtGui.QFont('Calibri', 16)
         self.now_playing_label = QtWidgets.QLabel('Now Playing: N/A')
         self.now_playing_label.setFont(now_playing_font)
+        self.now_playing_label.setAlignment(QtCore.Qt.AlignHCenter)
 
         self.main_layout.addWidget(self.now_playing_label)
         self.setLayout(self.main_layout)
