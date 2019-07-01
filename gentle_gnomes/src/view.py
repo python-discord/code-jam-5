651bd9913116c4aafbb6e1500b24c06ffed4335f
MarkKoz <KozlovMark@gmail.com>
2019-07-01 12:20:02 -0700

    Add basic form which relays results

--- gentle_gnomes/src/view.py
+++ gentle_gnomes/src/view.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 bp = Blueprint('view', __name__, url_prefix='/')
@@ -7,3 +7,9 @@ bp = Blueprint('view', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     return render_template('view/index.html')
+
+
+@bp.route('/search', methods=['POST'])
+def search():
+    location = request.form['location']
+    return render_template('view/results.html', results=location)
