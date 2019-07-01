0dae8398fd4c90beea9ecf6eaf18a1bad85c7d0c
zebralt <jocelyn.vernay@ipsos.com>
2019-07-01 21:21:25 +0200

    Add postprocess step to resource fetching

--- only_otters/ads/ad_resource.py
+++ only_otters/ads/ad_resource.py
@@ -2,20 +2,38 @@ import requests
 from lxml import html
 from dataclasses import dataclass
 import json
+import re
+from functools import partial, wraps
 
 from cachetools.func import ttl_cache, lru_cache
 
 
+def one_or_many(items: list, default=''):
+    if items:
+        if len(items) == 1:
+            return items[0]
+        return items
+    return default
+
 @lru_cache()
 def cached_xpath(tree, query):
-    return tree.xpath(query)
+    return one_or_many(tree.xpath(query))
 
 
 @ttl_cache(ttl=15)
-def cached_request(url):
+def cached_request(source):
     # TODO: Enable scraping through selenium for dynamic pages
-    response = requests.get(url)
+    response = requests.get(source)
+    with open('ff.f', 'w+') as f:
+        f.write(response.content.decode('utf-8'))
     return html.fromstring(response.content), response
+
+
+def dictquery(element, query):
+    return {
+        key: cached_xpath(element, expr)
+        for key, expr in query.items()
+    }
 
 
 @dataclass
@@ -24,49 +42,132 @@ class Resource:
     An interface to describe a remote resource that can be hot-retrieved.
     """
 
-    url: str
-    pattern: dict
-    location: str = None
+    source: str
+    query: dict
+    container: str = None
+    postprocess: "list of tuples (key => action)" = None
 
-    def __call__(self):
+    def fetch(self):
 
-        root, _ = cached_request(self.url)
+        root, _ = cached_request(self.source)
 
         if root is None:
             raise UserWarning  # TODO: Add a proper exception
 
-        if self.location is not None:
-            roots = cached_xpath(root, self.location)
+        if self.container is not None:
+            roots = cached_xpath(root, self.container)
 
             if not roots:
                 raise UserWarning  # TODO: Add a proper exception
 
             for element in roots:
-                yield {
-                    key: cached_xpath(element, expr)
-                    for key, expr in self.pattern.items()
-                }
+                yield dictquery(element, self.query)
 
         else:
-            return {
-                key: cached_xpath(root, expr)
-                for key, expr in self.pattern.items()
-            }
+            return dictquery(root, self.query)
+
+
+    def __call__(self):
+
+        fetched = self.fetch()
+
+        if self.postprocess is None:
+            return fetched
+
+        return map(self.postprocess, fetched)
+
+        # steps = []
+        # for step in self.postprocess:
+
+        #     target, action = step
+        #     eligible_keys = []
+
+        #     if target in self.query.keys():
+        #         eligible_keys = [target]
+        #     else:
+        #         eligible_keys = list(filter(partial(re.match, target), self.query))
+
+        #     steps.append({
+        #         'keys': eligible_keys,
+        #         'action': action
+        #     })
+
+
+        # for item in fetched:
+        #     for step in steps:
+        #         for key in step['keys']:
+        #             try:
+        #                 item[key] = step['action'](item[key])
+        #             except TypeError:
+        #                 print(item, key, item[key])
+        #                 raise
+        #     yield item
+
+        ####
+
+        # for step in self.postprocess:
+
+        #     for stepkey, fnct in step.items():
+
+        #         if stepkey in self.query:
+        #             for item in fetched:
+        #                 item[stepkey] = fnct(item[stepkey])
+
+        #         else:
+        #             eligible_keys = filter(partial(re.match, stepkey), self.query)
+        #             for key in eligible_keys:
+        #                 for item in fetched:
+        #                     item[stepkey] = fnct(item[stepkey])
+
+        # return fetched
+
+
+def safestrmanip(*excpts):
+    excpts = excpts or (IndexError,)
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(text):
+            try:
+                return fn(text)
+            except excpts as e:
+                print(e)
+                return text
+        return wrapper
+    return decorator
+
+
+def postprocess(item):
+
+    for key in item:
+        item[key] = item[key].strip()
+
+    try:
+        item['cursor'] = item['cursor'].split(',')[1]
+        item['start'] = item['start'].split(',')[0].split('(')[1]
+    except IndexError:
+        pass
+        item['cursor'] = None
+        item['start'] = None
+
+    return item
 
 
 if __name__ == "__main__":
 
     r = Resource(
-        url='https://www.theworldcounts.com/themes/our_environment',
-        location='//div[@id="main"]//a[@class="counter-link"]/div[@class="row"]',
-        pattern={
-            'figure': 'div[1]/div/p/text()',
-            'name': 'div[2]/div[@class="counter-title"]/h3/text()',
-            'happening': 'div[2]/div[@class="counter-title"]/span/text()'
-        }
+        source='https://www.theworldcounts.com/themes/our_environment',
+        container='//div[@id="main"]//a[@class="counter-link"]/div[@class="row"]',
+        query={
+            # 'figure': 'div[1]/div[@class="counter-number"]/p/text()',
+            'title': 'div[2]/div[@class="counter-title"]/h3/text()',
+            'subtitle': 'div[2]/div[@class="counter-title"]/span/text()',
+            'start': 'div[1]/div[@class="counter-number"]/script/text()',
+            'cursor': 'div[1]/div[@class="counter-number"]/script/text()'
+        },
+        postprocess=postprocess
     )
 
-    pulled_resources = *r(),
+    pulled_resources = [*r()]
 
     print(r)
     json.dump(
