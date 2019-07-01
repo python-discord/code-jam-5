1d6842e1ea2c28cb1795dec72612ec200cc9964a
Kieran Flaherty <kieranflaherty1995@gmail.com>
2019-07-01 22:49:54 +0100

    Got a simple form post up and running

--- practical_porcupines/flask_webportal/app.py
+++ practical_porcupines/flask_webportal/app.py
@@ -1,17 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from .forms import DatePickerForm
 from flask_bootstrap import Bootstrap
+from practical_porcupines.utils import ConfigApi
 import os
 
 flask_webportal_app = Flask(__name__)
 Bootstrap(flask_webportal_app)
-flask_webportal_app.config['SECRET_KEY'] = os.environ.get('API_SECRET_KEY')
+config_api = ConfigApi()
+flask_webportal_app.config['SECRET_KEY'] = config_api.SECRET_KEY
 
 @flask_webportal_app.route("/")
 def index():
     date_picker_form = DatePickerForm()
     return render_template("index.html", form=date_picker_form)
 
+@flask_webportal_app.route("/", methods=['POST'])
+def form_post():
+    start_date = request.form.get('start_date')
+    end_date = request.form.get('end_date')
+    api_url = f'http://{config_api.API_DOMAIN}:{config_api.API_PORT}'
+    return redirect(url_for('index'))
 
 @flask_webportal_app.route("/about")
 def about():
--- practical_porcupines/flask_webportal/forms.py
+++ practical_porcupines/flask_webportal/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import DateField
+from wtforms import DateField, SubmitField
 
 class DatePickerForm(FlaskForm):
     start_date = DateField(label="Start Date", id="startdate", format="%Y-%d-%m")
     end_date = DateField(label="End Date", id="enddate", format="%Y-%d-%m")
+    submit  = SubmitField(label="Submit")
