0dcb041e62ac2e51558736e4a37ff6d1c85d9a1b
MarkKoz <KozlovMark@gmail.com>
2019-07-01 13:03:57 -0700

    Send a request to Azavea API
    * Add requests as a dependency
    * Enable instance relative config (for specifying the API token)

--- gentle_gnomes/src/__init__.py
+++ gentle_gnomes/src/__init__.py
@@ -4,7 +4,7 @@ from . import view
 
 
 def create_app(test_config=None):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
 
     if test_config is None:
--- gentle_gnomes/src/view.py
+++ gentle_gnomes/src/view.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
+import requests
+from flask import current_app as app, Blueprint, render_template, request
 
+BASE_URL = 'https://app.climate.azavea.com'
 
 bp = Blueprint('view', __name__, url_prefix='/')
 
@@ -12,4 +14,21 @@ def index():
 @bp.route('/search', methods=['POST'])
 def search():
     location = request.form['location']
-    return render_template('view/results.html', results=location)
+    headers = {'Authorization': f'Token {app.config["AZAVEA_TOKEN"]}'}
+
+    cities = requests.get(f'{BASE_URL}/api/city', headers=headers, params={'name': location})
+    cities.raise_for_status()
+
+    cities = cities.json()
+    if cities['count'] < 1:
+        return render_template('view/results.html', results='City not found.')
+
+    city_id = cities['features'][0]['id']
+
+    hwi_url = f'{BASE_URL}/api/climate-data/{city_id}/RCP85/indicator/total_precipitation/'
+    hwi = requests.get(hwi_url, headers=headers)
+    hwi.raise_for_status()
+
+    hwi = hwi.json()['data']
+
+    return render_template('view/results.html', results=repr(hwi))
