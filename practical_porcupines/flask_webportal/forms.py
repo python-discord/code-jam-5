753726a075e1521be1212ae0dd9a94e1d897b7a4
Kieran Flaherty <kieranflaherty1995@gmail.com>
2019-07-01 18:33:06 +0100

    Date Picker working on front end, not the prettiest but will do for now, can refine later, added jQuery UI for this

--- practical_porcupines/flask_webportal/forms.py
+++ practical_porcupines/flask_webportal/forms.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import DateField
 
 class DatePickerForm(FlaskForm):
-    start_date = DateField(label="Start Date", id="startdate")
-    end_date = DateField(label="End Date", id="enddate")
+    start_date = DateField(label="Start Date", id="startdate", format="%Y-%d-%m")
+    end_date = DateField(label="End Date", id="enddate", format="%Y-%d-%m")
