830bb7138f03c7e12a37ea368c887c342ef35321
Kieran Flaherty <kieranflaherty1995@gmail.com>
2019-07-01 17:58:56 +0100

    * added flask-wtf to pipfile * added flask-bootstrap * passed form through to controller function

--- practical_porcupines/flask_webportal/app.py
+++ practical_porcupines/flask_webportal/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, render_template
 from .forms import DatePickerForm
+from flask_bootstrap import Bootstrap
+import os
 
 flask_webportal_app = Flask(__name__)
-
+Bootstrap(flask_webportal_app)
+flask_webportal_app.config['SECRET_KEY'] = os.environ.get('API_SECRET_KEY')
 
 @flask_webportal_app.route("/")
 def index():
-    return render_template("index.html")
+    date_picker_form = DatePickerForm()
+    return render_template("index.html", form=date_picker_form)
 
 
 @flask_webportal_app.route("/about")
 def about():
     return render_template("about.html")
-
 
 @flask_webportal_app.errorhandler(404)
 def error_404(e):
--- practical_porcupines/flask_webportal/forms.py
+++ practical_porcupines/flask_webportal/forms.py
@@ -2,5 +2,5 @@ from flask_wtf import FlaskForm
 from wtforms import DateField
 
 class DatePickerForm(FlaskForm):
-    start_date = DateField(label="Start Date")
-    end_date = DateField(label="End Date")
+    start_date = DateField(label="Start Date", id="startdate")
+    end_date = DateField(label="End Date", id="enddate")
