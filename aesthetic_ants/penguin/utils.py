6a920c15dbd9e81d26abaa78dfeea5040613e55e
Bast0006 <bast@fastmail.com>
2019-07-02 00:15:19 -0700

    Add distance between coordinates functions

--- aesthetic_ants/penguin/utils.py
+++ aesthetic_ants/penguin/utils.py
@@ -14,6 +14,22 @@ def angle_between(x1: float, y1: float, x2: float, y2: float) -> float:
     return -math.atan2(dy, dx)
 
 
+def distance_between(x1: float, y1: float, x2: float, y2: float) -> float:
+    """
+    Returns the distance between the two points (x1, y1) and (x2, y2)
+    """
+    return distance_between_sq(x1, y1, x2, y2)**0.5
+
+
+def distance_between_sq(x1: float, y1: float, x2: float, y2: float) -> float:
+    """
+    Returns the squared distance between the two points (x1, y1) and (x2, y2)
+    """
+    dx = x2 - x1
+    dy = y2 - y1
+    return dx**2 + dy**2
+
+
 loader = pyglet.resource.Loader(path="../resources")
 
 keys = pyglet.window.key.KeyStateHandler()
