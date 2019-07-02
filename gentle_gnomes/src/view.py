0b0a3616f70231505ed5c97abea55adb2fde76b2
Noah <noah@coronasoftware.net>
2019-07-02 13:39:28 +0000

    Improve web page layout and style
    Add Bootstrap to make things easier.
    
    Render `index.html` for `/search` as well. Including it is a little more
    flexible than having it as it's own page. We may still want a dedicated results
    page, but for now we can just reuse the index page.

--- gentle_gnomes/src/view.py
+++ gentle_gnomes/src/view.py
@@ -1,4 +1,4 @@
-from flask import current_app as app, Blueprint, render_template, request
+from flask import current_app as app, Blueprint, render_template, request, flash
 
 from . import data
 
@@ -15,9 +15,11 @@ def search():
     location = request.form['location']
 
     city = app.azavea.get_city_id(location)
-    if not city:
-        return render_template('view/results.html', results='City not found.')
+    if city:
+        top = data.get_top_indicators(app.azavea, city)
+        results = repr(top)
+    else:
+        flash('Location not found.')
+        results = None
 
-    top = data.get_top_indicators(app.azavea, city)
-
-    return render_template('view/results.html', results=repr(top))
+    return render_template('view/index.html', results=results)
