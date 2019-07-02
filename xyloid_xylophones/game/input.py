ae89ce617520d0149cf35991cd6c84c4f98ed168
Tom Casavant <tfcasavant@gmail.com>
2019-07-02 15:23:03 -0400

    When multiple keys are pressed moves user to open position

--- xyloid_xylophones/game/input.py
+++ xyloid_xylophones/game/input.py
@@ -4,7 +4,7 @@ from config import current_zone
 import pyglet
 
 def handle_input():
-    # print('old player: %s %s' % (player.x, player.y))
+    '''Moves the player to a new position if allowed'''
     new_x = player.x
     new_y = player.y
     if pyglet.window.key.UP in keys:
@@ -15,13 +15,21 @@ def handle_input():
         new_x -= 1
     if pyglet.window.key.RIGHT in keys:
         new_x += 1
-    permitted = checkCollision(new_x, new_y)
-    if permitted:
-        player.x = new_x
-        player.y = new_y
-        # print('new player: %s, %s' % (player.x, player.y))
+
+    if checkCollision(new_x, new_y):
+        move(new_x, new_y) #Player moves to the new position
+    elif checkCollision(player.x, new_y):
+        move(player.x, new_y) #Player collided with something on the x-axis
+    elif checkCollision(new_x, player.y):
+        move(new_x, player.y) #Player collided with something on y-axis
+
+def move(new_x, new_y):
+    '''Changes players position'''
+    player.x = new_x
+    player.y = new_y
 
 def checkCollision(new_x, new_y):
+    '''Verifies that the character is allowed to move to this position'''
     query_x = -1024 + (new_x * player.width)
     query_y = -1024 + (new_y * player.height)
     # print('queried %s,%s' % (query_x, query_y))
