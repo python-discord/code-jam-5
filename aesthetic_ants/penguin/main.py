0b91d4e84112be154f4c3578363007420145462b
kozzza <kozyboy13@gmail.com>
2019-07-01 21:14:13 -0700

    Revert "resolved issues"
    This reverts commit 5d6751835143b4da9327079499013b6ed9512f8d.

--- aesthetic_ants/penguin/main.py
+++ aesthetic_ants/penguin/main.py
@@ -1,26 +1,22 @@
 import pyglet
-from enemy import Enemy
+from .enemy import Enemy
 
 window = pyglet.window.Window(caption='Penguin Snowball')
 pyglet.resource.path = ['../resources']
 pyglet.resource.reindex()
-enemy_image = pyglet.resource.image('circle.png')
-enemy_image.width = 50
-enemy_image.height = 50
 
-
-enemy = Enemy(enemy_image)
-
-
-def main():
-    pyglet.clock.schedule_interval(enemy.update, 1/120)
-    pyglet.app.run()
+enemy = Enemy()
 
 
 @window.event
 def on_draw():
     window.clear()
     enemy.draw()
+
+
+def main():
+    pyglet.clock.schedule_interval(enemy.update, 1/120)
+    pyglet.app.run()
 
 
 if __name__ == '__main__':
