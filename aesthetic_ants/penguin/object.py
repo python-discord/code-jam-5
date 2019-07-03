00c8b337135f1d4f00d64516d3e7b089ebf2aeab
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2019-07-02 21:39:50 -0700

    Add an "Object Space"
    Useful for collecting all objects and updating them accordingly. also
    includes a way to manually add collision handlers.

---
+++ aesthetic_ants/penguin/object.py
@@ -0,0 +1,29 @@
+import weakref
+
+import pyglet
+
+class Object:
+    """Base class for all objects"""
+
+    space = None
+    collision_type = None
+
+    def add_to_space(self, space):
+        self.space = weakref.proxy(space)
+
+    def remove_from_space(self):
+        self.space = None
+
+    def update(self, dt):
+        pass
+
+class PhysicalObject(Object, pyglet.sprite.Sprite):
+    """Base class for all tangible objects (objects with sprites)"""
+
+    def add_to_space(self, space):
+        self.batch = space.batch
+        super().add_to_space(space)
+
+    def remove_from_space(self):
+        self.batch = None
+        super().remove_from_space()
