2169648ea90f4ca6f7860f5168c3254905a72f94
MarkKoz <KozlovMark@gmail.com>
2019-07-02 17:44:13 -0700

    Create an indicator class
    The class contains additional info such as the label, description,
    units, and the city.
    
    * Use np.empty() instead of np.zeros()
    * Rename slope to rate

---
+++ gentle_gnomes/src/indicator.py
@@ -0,0 +1,65 @@
+from collections import Counter
+from itertools import chain
+from typing import Tuple
+
+import numpy as np
+from scipy import stats
+
+from .azavea import City, Client
+
+
+INDICATORS = (
+    'heat_wave_incidents',
+    'total_precipitation',
+)
+
+
+class Indicator:
+    def __init__(self, name: str, city: City, client: Client):
+        self.name = name
+        self.city = city
+        self._client = client
+
+        self._populate_data()
+
+    def _populate_data(self):
+        items = []
+        count = 0
+
+        for scenario in ('historical', 'RCP85'):
+            response = self._client.get_indicator_data(self.city.id, scenario, self.name)
+            self.label = response['indicator']['label']
+            self.description = response['indicator']['description']
+            self.units = response['units']
+
+            items.append(response['data'].items())
+            count += len(response['data'])
+
+        x = np.empty(count, dtype=np.dtype(int))
+        y = np.empty(count, dtype=np.dtype(float))
+
+        for i, data in enumerate(chain.from_iterable(items)):
+            year, values = data
+            x[i] = int(year)
+            y[i] = values['avg']
+
+        self.rate = self._calc_slope(x, y)
+
+    @staticmethod
+    def _calc_slope(x: np.ndarray, y: np.ndarray) -> float:
+        slope, *_ = stats.linregress(x, y)
+        return slope
+
+
+def get_top_indicators(city: City, client: Client, n: int = 5) -> Tuple[Indicator, ...]:
+    """Return the top n indicators with the highest rate of change."""
+    rates = Counter()
+    indicators = {}
+
+    for name in INDICATORS:
+        indicator = Indicator(name, city, client)
+
+        rates[name] = indicator.rate
+        indicators[name] = indicator
+
+    return tuple(indicators[k] for k, _ in rates.most_common(n))
--- gentle_gnomes/src/view.py
+++ gentle_gnomes/src/view.py
@@ -1,6 +1,6 @@
 from flask import current_app as app, Blueprint, render_template, request, flash
 
-from . import data
+from . import indicator
 
 bp = Blueprint('view', __name__, url_prefix='/')
 
@@ -16,7 +16,7 @@ def search():
 
     city = app.azavea.get_city_id(location)
     if city:
-        top = data.get_top_indicators(app.azavea, city)
+        top = indicator.get_top_indicators(app.azavea, city)
         results = repr(top)
     else:
         flash('Location not found.')
