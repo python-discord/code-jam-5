0d371a80078f877353767fba217c975b9db86e83
Kieran Flaherty <kieranflaherty1995@gmail.com>
2019-07-03 18:46:15 +0100

    wip : web_portal
    * Replaced all ' for "
    * Removed unnecessary imports
    * Imported DateFormatError into flask_webportal.utils as it was missing
    * Created an api module in flask_webportal (yet to split out the code)
    * Realised I was using incorrect date_format (D'oh) fixed now
    * Minor front end adjustments
    
    todo:
    
    * Modularise API request
    * Display result of request in front end
    * Format code correctly (flake8) - might need help with this

--- practical_porcupines/flask_webportal/app.py
+++ practical_porcupines/flask_webportal/app.py
@@ -1,37 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for,flash
 from .forms import DatePickerForm
-from flask_bootstrap import Bootstrap
 from practical_porcupines.utils import ConfigApi
 import requests
-import os
 
 flask_webportal_app = Flask(__name__)
-Bootstrap(flask_webportal_app)
 config_api = ConfigApi()
-flask_webportal_app.config['SECRET_KEY'] = config_api.SECRET_KEY
+flask_webportal_app.config["SECRET_KEY"] = config_api.SECRET_KEY
 
-@flask_webportal_app.route("/",methods=['GET','POST'])
+@flask_webportal_app.route("/",methods=["GET","POST"])
 def index():
     date_picker_form = DatePickerForm()
-    if request.method == 'GET':
+    if request.method == "GET":
         return render_template("index.html", form=date_picker_form)
     if date_picker_form.validate_on_submit():
-        start_date = request.form.get('start_date') 
+        start_date = request.form.get("start_date") 
         start_date_time = start_date + " 00:00:00"
-        end_date = request.form.get('end_date')
+        end_date = request.form.get("end_date")
         end_date_time = end_date + " 00:00:00"
-        api_url = f'http://{config_api.API_DOMAIN}:{config_api.API_PORT}'
+        api_url = f"http://{config_api.API_DOMAIN}:{config_api.API_PORT}"
         try:
-            request_body = { "times": [
-                start_date_time,
-                end_date_time
-             ] }
-            api_response = requests.get(api_url,data=request_body).json()['body']
-            wl_difference = api_response['wl_difference']
-            wl_string = f'The difference in GMSL between {start_date} and {end_date} was {wl_difference}mm'
-            return redirect(url_for('index'), wl_string=wl_string)
-        except:
-            flash('Error sending request to API')
+            request_body = {
+                "date_1":start_date_time,
+                "date_2":end_date_time
+            }
+            api_response = requests.get(api_url,data=request_body).json()
+            wl_difference = api_response['body']["wl_difference"]
+            wl_string = f"The difference in GMSL between {start_date} and {end_date} was {wl_difference}mm"
+            return redirect(url_for("index"), wl_string=wl_string)
+        except Exception as e:
+            flash("Error sending request to API")
             return render_template("index.html", form=date_picker_form)
     else:
         flash("Invalid Dates!")
--- practical_porcupines/flask_webportal/forms.py
+++ practical_porcupines/flask_webportal/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import DateField, SubmitField
 
 class DatePickerForm(FlaskForm):
-    start_date = DateField(label="Start Date", id="startdate", format="%Y-%d-%m")
-    end_date = DateField(label="End Date", id="enddate", format="%Y-%d-%m")
+    start_date = DateField(label="Start Date", id="startdate", format="%Y-%m-%d")
+    end_date = DateField(label="End Date", id="enddate", format="%Y-%m-%d")
     submit  = SubmitField(label="Submit")
 
     def validate_on_submit(self):
