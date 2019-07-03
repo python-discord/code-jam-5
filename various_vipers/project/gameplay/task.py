8b753e111da0d0d8ad6fa2b55cbbd91c055bbf8e
Horiwix <auseviciusss@gmail.com>
2019-07-04 00:06:25 +0300

    Now able to click on task tiles to open and close task window.

--- various_vipers/project/gameplay/task.py
+++ various_vipers/project/gameplay/task.py
@@ -1,6 +1,12 @@
 import logging
 from typing import Optional
 
+import pygame as pg
+from pygame.image import load
+
+from project.UI.element.button import Button
+from project.constants import BUTTONS, Color, HEIGHT, WIDTH
+from . import set_open_task
 from .biome import Biome, BiomeCity, BiomeDesert, BiomeForest, BiomeMountains
 
 
@@ -10,7 +16,29 @@ logger = logging.getLogger(__name__)
 class Task(object):
     """Generic class for game Task on earth."""
 
-    def __init__(self, biome: Optional[Biome] = None):
+    # Marked when task is completed or closed, so it can be deleted
+    is_done: bool = False
+
+    hover_close: bool = False
+
+    def __init__(self, screen: pg.Surface, biome: Optional[Biome] = None):
+        self.screen = screen
+
+        self.fill_rect = pg.Rect(
+            int(WIDTH * 0.1), int(HEIGHT * 0.1), int(WIDTH * 0.8), int(HEIGHT * 0.8)
+        )
+
+        button_size = 60
+        self.close_btn = Button(
+            self.screen,
+            x=self.fill_rect.x + self.fill_rect.width - button_size - 5,
+            y=self.fill_rect.y + 5,
+            width=button_size,
+            height=button_size,
+            image=load(str(BUTTONS["close-btn"])).convert_alpha(),
+            image_hover=load(str(BUTTONS["close-btn-hover"])).convert_alpha(),
+        )
+
         if isinstance(biome, BiomeCity):
             logger.debug(f"Generating city themed task of type {type(self)}")
         elif isinstance(biome, BiomeDesert):
@@ -22,7 +50,23 @@ class Task(object):
 
     def start(self) -> None:
         """Start playing the task."""
+        set_open_task(self)
         logger.debug("Starting task.")
+
+    def update(self, event: pg.event) -> None:
+        """Update is called every game tick."""
+        if self.close_btn.rect.collidepoint(pg.mouse.get_pos()):
+            self.hover_close = True
+            if event.type == pg.MOUSEBUTTONDOWN:
+                set_open_task(None)
+                self.is_done = True
+        else:
+            self.hover_close = False
+
+    def draw(self) -> None:
+        """Draw is called every game tick."""
+        self.screen.fill(Color.black, self.fill_rect)
+        self.close_btn.draw(hover=self.hover_close)
 
 
 class TaskCursorMaze(Task):
