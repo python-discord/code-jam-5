183f3524121da31946ccc179a52f7061d379f637
zebralt <jocelyn.vernay@ipsos.com>
2019-07-03 18:03:09 +0200

    Add attempt at dataqobject decorator

--- only_otters/ads/qtobjcounter.py
+++ only_otters/ads/qtobjcounter.py
@@ -1,7 +1,7 @@
 import sys
 from dataclasses import dataclass
 import inspect
-from functools import partial
+from functools import partial, wraps
 import copy
 
 # https://stackoverflow.com/questions/49479603/generating-pyqtproperty-methods
@@ -9,69 +9,20 @@ import copy
 # https://stackoverflow.com/questions/52290153/qml-unable-to-assign-undefined-to
 # https://www.riverbankcomputing.com/static/Docs/PyQt5/qt_properties.html
 
-from PyQt5.QtCore import pyqtProperty, QCoreApplication, QObject, QUrl
-from PyQt5.QtQml import qmlRegisterType, QQmlComponent, QQmlEngine
+from PyQt5.QtCore import pyqtProperty, pyqtSignal, QObject
+
+from dataqobject import dataqobject
 
 
-def resolve_type(type_):
-    return {
-        str: 'QString'
-    }.get(type_, type_)
+@dataqobject
+class Counter:
+
+    value: float
+    offset: float
+    interval: int = 1000
+    precision: int = 0
 
 
-def make_getter(arg, type_):
-
-    print(arg, '=>', type_)
-
-    type_ = resolve_type(type_)
-
-    # return pyqtProperty(type_)
-    # return (
-    #     partial(getattr, name=arg)
-    # )
-    return lambda self: getattr(self, arg)
-
-
-def dataqobject(cls):
-
-    # cls = copy.copy(cls)
-    
-    d = inspect.getfullargspec(cls.__init__)
-
-    for arg in d.args[1:]:
-        annotation = d.annotations[arg]
-        getter = make_getter('_' + arg, annotation)
-        setattr(cls, arg, getter)
-        print(getattr(cls, arg))
-
-    return cls
-
-
-class QDataObject(QObject):
-
-    def __init__(self):
-        super().__init__()
-
-
-
-# @dataqobject
-# @dataclass
-# class Counter(QDataObject):
-
-#     value: float
-#     offset: float
-#     interval: int = 1000
-#     precision: int = 0
-
-#     @pyqtProperty(float)
-#     def _value(self):
-#         return value
-
-#     @pyqtProperty(float)
-#     def _offset(self)
-
-
-# @dataclass
 class FactCounter(QObject):
 
     # value: float
@@ -103,18 +54,29 @@ class FactCounter(QObject):
     def precision(self):
         return self._precision
 
+    @property
+    def x(self):
+        return "bb"
 
-# if __name__ == "__main__":
 
-#     # Counter.x = lambda self: self.value
+FactCounter.x = pyqtProperty('QString',
+    constant=True,
+    fget=lambda s: "aa"
+)
+
+# FactCounter.x = 
+
+
+if __name__ == "__main__":
+
+    # Counter.x = lambda self: self.value
     
-#     c = Counter(
-#         value=3,
-#         offset=2
-#     )
+    c = Counter(
+        _value=3,
+        _offset=2
+    )
 
-#     print(c)
-
-#     print(getattr(Counter, '_value'))
-#     print(Counter._value)
-#     print(c._value())
+    print(Counter)
+    print(dir(c))
+    print(c)
+    print(c.value)
