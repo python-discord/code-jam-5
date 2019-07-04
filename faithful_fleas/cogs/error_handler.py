f03d8460b6a2b01cdb7309948ae8d71e0e7ff7e9
Rohan <icemam449@gmail.com>
2019-07-04 09:48:00 +0530

    ran the linter and made changes

--- faithful_fleas/cogs/error_handler.py
+++ faithful_fleas/cogs/error_handler.py
@@ -19,26 +19,33 @@ class ErrorHandler(commands.Cog):
         """Task when an error occurs."""
 
         if isinstance(error, commands.CommandNotFound):
-            return logger.info(f"{ctx.author} used {ctx.message.content} but nothing was found.")
+            return logger.info(f"{ctx.author} used {ctx.message.content} "
+                               f"but nothing was found.")
 
         if isinstance(error, commands.MissingRequiredArgument):
-            logger.info(f"{ctx.author} called {ctx.message.content} and triggered MissingRequiredArgument error.")
+            logger.info(f"{ctx.author} called {ctx.message.content} and "
+                        f"triggered MissingRequiredArgument error.")
             return await ctx.send(f"`{error.param}` is a required argument.")
 
         if isinstance(error, commands.CheckFailure):
-            logger.info(f"{ctx.author} called {ctx.message.content} and triggered CheckFailure error.")
+            logger.info(f"{ctx.author} called {ctx.message.content} and triggered"
+                        f" CheckFailure error.")
             return await ctx.send("You do not have permission to use this command!")
 
         if isinstance(error, (commands.UserInputError, commands.BadArgument)):
-            logger.info(f"{ctx.author} called {ctx.message.content} and triggered UserInputError error.")
+            logger.info(f"{ctx.author} called {ctx.message.content} and triggered"
+                        f" UserInputError error.")
             return await ctx.send("Invalid arguments.")
 
         if isinstance(error, commands.CommandOnCooldown):
-            logger.info(f"{ctx.author} called {ctx.message.content} and triggered ComamndOnCooldown error.")
-            return await ctx.send(f"Command is on cooldown! Please retry after `{error.retry_after}`")
+            logger.info(f"{ctx.author} called {ctx.message.content} and"
+                        f" triggered ComamndOnCooldown error.")
+            return await ctx.send(f"Command is on cooldown!"
+                                  f" Please retry after `{error.retry_after}`")
 
         if isinstance(error, commands.BotMissingPermissions):
-            logger.info(f"{ctx.author} called {ctx.message.content} and triggered BotMissingPermissions error.")
+            logger.info(f"{ctx.author} called {ctx.message.content} and triggered"
+                        f" BotMissingPermissions error.")
             embed = discord.Embed()
             embed.colour = discord.Colour.blue()
             title = "The bot lacks the following permissions to execute the command:"
@@ -49,10 +56,12 @@ class ErrorHandler(commands.Cog):
             return await ctx.send(embed=embed)
 
         if isinstance(error, commands.DisabledCommand):
-            logger.info(f"{ctx.author} called {ctx.message.content} and triggered DisabledCommand error.")
+            logger.info(f"{ctx.author} called {ctx.message.content} and"
+                        f" triggered DisabledCommand error.")
             return await ctx.send("The command has been disabled!")
 
-        logger.warning(f"{ctx.author} called {ctx.message.content} and triggered the following error:\n {error}")
+        logger.warning(f"{ctx.author} called {ctx.message.content} and"
+                       f" triggered the following error:\n {error}")
 
 
 def setup(bot):
