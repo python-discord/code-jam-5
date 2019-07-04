f9c8dc273912d51209152665df4064830f8e8589
Phyro <phyrophantom@gmail.com>
2019-07-04 12:59:20 -0500

    Added wind turbine and solar panel graphics.  TODO: the font is not very readable over these images, either in black or white.  I think the text should probably be moved to the side or above/below, or given an outline.

--- combined_crusaders/machines.py
+++ combined_crusaders/machines.py
@@ -14,13 +14,14 @@ class Machine(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.font = pygame.font.Font(None, 20)
         self.name = name
+        self.text_color = Color('black')
         self.price = price
         self.energy_per_second = energy_per_second
         self._count = 0
         self.image = image
         self.rect = self.image.get_rect(center=location)
         self.count_sprite = pygame.sprite.Sprite()
-        self.count_sprite.image = self.font.render("9", 0, Color('black'))
+        self.count_sprite.image = self.font.render("9", 0, self.text_color )
         self.count_sprite.rect = self.count_sprite.image.get_rect(
             center=location)
         self.count = 0
@@ -32,7 +33,7 @@ class Machine(pygame.sprite.Sprite):
     @count.setter
     def count(self, value):
         self._count = value
-        count_text = self.font.render(str(value), True, Color('black'))
+        count_text = self.font.render(str(value), True, self.text_color)
         self.count_sprite.image = count_text
 
 
