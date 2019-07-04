7674b006729717dfe528665e8f1a0dfa4db2343a
Luckyme66 <51961563+Luckyme66@users.noreply.github.com>
2019-07-04 13:54:52 +0100

    A text version with no scores
    Feel free to edit or abolish this code.

---
+++ Flying_frogs/Car.py
@@ -0,0 +1,46 @@
+score = 0
+
+#set up car object
+class car(object):
+    electric = False
+    image = 'Big bulky car and fumes'
+
+    def __init__(self,electric,image):
+        self.electric = electric
+        self.image = image
+    #Method to ask user to smash the car
+    def smash(self):
+        while True:
+            answer = input('Is this car electric? (y/n): ')
+            if answer.lower() == 'n':
+                return True
+            elif answer.lower() == 'y':
+                return False
+            else:
+                print('Please respond with y or n.')
+    #Show the car
+    def show(self):
+        print(self.image)
+
+car1=car(False,'Big bulky car and fumes')
+car2=car(True,'Silent small green car')
+
+#Dictionary of cars. Value allows us to judge how often that car is guessed correctly
+cars={car1:0,car2:0}
+
+#Cycles through the cars and asking if electric or not and producing an output
+for i in cars:
+    i.show()
+    smashed=i.smash()
+    if smashed and i.electric:
+        print('Oh no! You smashed an electric car!')
+        cars[i]-=1
+    elif smashed and not i.electric:
+        print('Well done! You smashed a bad car!')
+        cars[i]+=1
+    elif not smashed and i.electric:
+        print('Well done! You let an electric car go!')
+        cars[i]+=1
+    elif not smashed and not i.electric:
+        print('Oh no! You let a bad car go!')
+        cars[i]-=1
