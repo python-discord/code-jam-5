ea69704ff3a1f4645c90064a9a3a4e7d9cead0f4
Bast0006 <bast@fastmail.com>
2019-07-05 07:00:38 -0700

    Create a new GameOverScreen object to display when the game ends

--- aesthetic_ants/penguin/game.py
+++ aesthetic_ants/penguin/game.py
@@ -2,6 +2,7 @@ import pyglet
 
 from .constants import CollisionType
 from .enemy import Enemy
+from .object import Object
 from .player import Player
 from .space import Space
 from .utils import keys
@@ -52,3 +53,16 @@ class Game(pyglet.window.Window):
 
         pyglet.clock.schedule_interval(self.update, 1/self.fps)
         pyglet.app.run()
+class GameOverScreen(Object):
+    def __init__(self, window):
+        self.label = pyglet.text.Label("Game Over",
+                                       font_name="Times New Roman",
+                                       font_size=36,
+                                       x=window.width // 2,
+                                       y=window.height // 2,
+                                       anchor_x='center',
+                                       anchor_y='center')
+
+    def add_to_space(self, space):
+        super().add_to_space(space)
+        self.label.batch = space.batch
