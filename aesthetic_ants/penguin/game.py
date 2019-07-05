b7ed2ce3eb488cc062a7cd614ec0add1b430d8d0
Bast0006 <bast@fastmail.com>
2019-07-05 11:49:28 -0700

    Add UI layer and score to game

--- aesthetic_ants/penguin/game.py
+++ aesthetic_ants/penguin/game.py
@@ -4,11 +4,14 @@ from .constants import CollisionType
 from .enemy import Enemy
 from .object import Object
 from .player import Player
+from .snowball import Snowball
 from .space import Space
 from .utils import keys
 
 
 class Game(pyglet.window.Window):
+    score = 0
+
     def __init__(self, *, fps=120, **kwargs):
         super().__init__(caption='Penguin Snowball', **kwargs)
         self.is_over = False
@@ -25,6 +28,12 @@ class Game(pyglet.window.Window):
         self.push_handlers(self.player)
         self.push_handlers(keys)
 
+        # Create UI
+        self.ui_space = self.create_ui()
+
+        self.score_label = ScoreLabel(self)
+        self.ui_space.add(self.score_label)
+
     def create_space(self) -> Space:
         """Returns a space usable for the game"""
 
@@ -35,19 +44,28 @@ class Game(pyglet.window.Window):
                                     self.on_collision_player_enemy,
                                     Player.collides_with)
 
-        space.add_collision_handler(CollisionType.ENEMY,
-                                    CollisionType.SNOWBALL,
-                                    Enemy.on_collision_snowball,
-                                    Enemy.collides_with)
+        space.add_collision_handler(CollisionType.SNOWBALL,
+                                    CollisionType.ENEMY,
+                                    self.on_collision_snowball_enemy,
+                                    Snowball.collides_with)
+
+        return space
+
+    def create_ui(self) -> Space:
+        """Returns the user interface space"""
+
+        space = Space()
 
         return space
 
     def on_draw(self):
         self.clear()
         self.space.draw()
+        self.ui_space.draw()
 
     def update(self, dt):
         self.space.update(dt)
+        self.ui_space.update(dt)
 
     def run(self):
         """Runs the game"""
@@ -62,6 +80,13 @@ class Game(pyglet.window.Window):
             game_over_screen = GameOverScreen(self)
             self.space.add(game_over_screen)
 
+    def on_collision_snowball_enemy(self, snowball, enemy):
+        if not self.is_over:
+            self.score += enemy.score
+            self.score_label.label.text = str(self.score)
+
+        enemy.on_collision_snowball(snowball)
+
 
 class GameOverScreen(Object):
     def __init__(self, window):
@@ -72,6 +97,21 @@ class GameOverScreen(Object):
                                        y=window.height // 2,
                                        anchor_x='center',
                                        anchor_y='center')
+
+    def add_to_space(self, space):
+        super().add_to_space(space)
+        self.label.batch = space.batch
+
+
+class ScoreLabel(Object):
+    def __init__(self, window):
+        self.label = pyglet.text.Label("0",
+                                       font_name="Times New Roman",
+                                       font_size=18,
+                                       x=10,
+                                       y=window.height - 10,
+                                       anchor_x='left',
+                                       anchor_y='top')
 
     def add_to_space(self, space):
         super().add_to_space(space)
