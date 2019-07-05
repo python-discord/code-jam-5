c9d7c3ba21dd8a8398764dc92c65deade980ea33
mathsman5133 <mathsman5132@gmail.com>
2019-07-05 19:10:18 +1000

    Update Trivia Commands - Properly parse and validate arguments with converters - Add instance attribute containing a list of valid categories and difficulties - Add cog command error for argument parsing errors. - Add help/docstrings to commands + helper functions - Lower arguments before parsing and checking them

--- thoughtful_termites/bot/cogs/trivia.py
+++ thoughtful_termites/bot/cogs/trivia.py
@@ -2,11 +2,46 @@ import asyncio
 import discord
 import json
 import random
+import typing
 
 from datetime import datetime
 from discord.ext import commands
 
 TRIVIA_QUESTIONS_PATH = './resources/trivia_questions.json'
+
+
+class CategoryConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        """Ensures arguments are valid categories for `>trivia` commands.
+        """
+        cog = ctx.bot.get_cog('Trivia')
+        if not cog:
+            raise RuntimeError('Trivia cog not loaded.')
+
+        argument = argument.lower()
+        valid_categories = cog.trivia_categories
+
+        if argument not in valid_categories:
+            raise commands.BadArgument(f"Valid category not supplied. Try one of the following:\n" +
+                                       '\n'.join(f'•`{n}`' for n in valid_categories))
+        return argument
+
+
+class DifficultyConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        """Ensures arguments are valid difficulties for `>trivia` commands.
+        """
+        cog = ctx.bot.get_cog('Trivia')
+        if not cog:
+            raise RuntimeError('Trivia cog not loaded.')
+
+        argument = argument.lower()
+        valid_difficulties = cog.trivia_difficulties
+
+        if argument not in valid_difficulties:
+            raise commands.BadArgument(f"Valid difficulty not supplied. Try one of the following:\n" +
+                                       '\n'.join(f'•`{n}`' for n in valid_difficulties))
+        return argument
 
 
 class Trivia(commands.Cog):
@@ -16,18 +51,27 @@ class Trivia(commands.Cog):
         with open(TRIVIA_QUESTIONS_PATH) as fp:
             self.raw_trivia_questions = json.load(fp)
 
-        self.trivia_categories = set(n['category'] for n in self.raw_trivia_questions)
-        self.trivia_difficulties = set(n['difficulty'] for n in self.raw_trivia_questions)
+        self.trivia_categories = set(n['category'].lower() for n in self.raw_trivia_questions)
+        self.trivia_difficulties = set(n['difficulty'].lower() for n in self.raw_trivia_questions)
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.BadArgument):
+            return await ctx.send(str(error))
+        if isinstance(error, commands.BadUnionArgument):
+            await ctx.send('Error encountered with parameters passed! Please see examples for help...')
+            return await ctx.send_help(ctx.command)
 
     def get_trivia_question(self, difficulty, category):
+        """Returns a random trivia entry with given difficulty and category.
+        """
         random.shuffle(self.raw_trivia_questions)
 
         def pred(result):
             if category:
-                if not result['category'] == category:
+                if not result['category'].lower() == category:
                     return False
             if difficulty:
-                if not result['difficulty'] == difficulty:
+                if not result['difficulty'].lower() == difficulty:
                     return False
             return True
 
@@ -35,6 +79,8 @@ class Trivia(commands.Cog):
         return question
 
     def prepare_embed(self, difficulty, category):
+        """Returns a formatted discord embed, possible and correct answers for a trivia difficult and category.
+        """
         letters = ['A', 'B', 'C', 'D']
         results = self.get_trivia_question(difficulty, category)
 
@@ -52,6 +98,10 @@ class Trivia(commands.Cog):
         return embed, answers, results['correct_answer']
 
     async def do_trivia(self, ctx, difficulty, category):
+        """Run a single trivia question.
+        This function sends and adds reactions to a formatted embed, and waits for a response from the user.
+        It will then edit the message as appropriate if the answer is correct/incorrect, notifying the user the result.
+        """
         lookup = ['\N{REGIONAL INDICATOR SYMBOL LETTER A}',
                   '\N{REGIONAL INDICATOR SYMBOL LETTER B}',
                   '\N{REGIONAL INDICATOR SYMBOL LETTER C}',
@@ -86,14 +136,52 @@ class Trivia(commands.Cog):
         return correct
 
     @commands.group(invoke_without_command=True)
-    async def trivia(self, ctx, difficulty: str = None, *, category: str = None):
-        # TODO: Validate difficulty and category with custom converter
+    async def trivia(self, ctx, difficulty: typing.Union[DifficultyConverter, CategoryConverter] = None,
+                     *, category: CategoryConverter = None):
+        """Complete a single trivia round.
+
+        Parameters
+        -----------------
+        • Difficulty: a valid difficulty found with `>trivia difficulties`. This defaults to all difficulties.
+        • Category: a valid category found with `>trivia categories`. This defaults to include all categories.
+
+        Examples
+        -----------------
+        • `>trivia`
+        • `>trivia easy`
+        • `>trivia hard Animals`
+        • `>trivia "Entertainment: Japanese Anime & Manga"
+        • `>trivia Geography`
+        """
+        print(category, difficulty)
         if ctx.invoked_subcommand is not None:
             return
+
+        if difficulty not in self.trivia_difficulties:
+            category = difficulty
+            difficulty = None
+
         await self.do_trivia(ctx, difficulty, category)
 
     @trivia.command()
-    async def game(self, ctx, difficulty: str = None, *, category: str = None):
+    async def game(self, ctx, difficulty: typing.Union[DifficultyConverter, CategoryConverter] = None,
+                   *, category: CategoryConverter = None):
+        """Complete a trivia game with multiple rounds.
+        The bot will prompt you with how many rounds you wish to play. An appropriate answer would be `5`, or `20` etc.
+
+        Parameters
+        -----------------
+        • Difficulty: a valid difficulty found with `>trivia difficulties`. This defaults to all difficulties.
+        • Category: a valid category found with `>trivia categories`. This defaults to include all categories.
+
+        Examples
+        -----------------
+        • `>trivia`
+        • `>trivia easy`
+        • `>trivia hard Animals`
+        • `>trivia "Entertainment: Japanese Anime & Manga"
+        • `>trivia Geography`
+        """
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel
         msg = await ctx.send('How many rounds would you like to play? A number like `5`, or `20` would be appropriate.')
@@ -120,10 +208,11 @@ class Trivia(commands.Cog):
             else:
                 losses += 1
 
-        fmt = f'Game Over!\nAttempts: {attempts}\nCorrect: {wins}\n' \
+        fmt = f'Attempts: {attempts}\nCorrect: {wins}\n' \
               f'Incorrect: {losses}\nCorrect: {int(wins/attempts*100)}%\n\n' \
               f'Thanks for playing!'
         await ctx.send(embed=discord.Embed(colour=self.bot.colour,
+                                           title='Game Over!',
                                            description=fmt,
                                            timestamp=datetime.utcnow()
                                            )
@@ -131,6 +220,18 @@ class Trivia(commands.Cog):
 
     @trivia.command()
     async def categories(self, ctx):
+        """Returns a list of all trivia categories and difficulties.
+
+        Example
+        -------------
+        • `>trivia categories`
+        • `>trivia difficulties`
+
+        Aliases
+        --------------
+        • `>trivia categories` (primary)
+        • `>trivia difficulties`
+        """
         fmt = 'Categories:\n' + '\n'.join(f'•`{n}`' for n in self.trivia_categories)
         fmt += f"\n\nDifficulties:\n" + '\n'.join(f'•`{n}`' for n in self.trivia_difficulties)
         await ctx.send(embed=discord.Embed(colour=self.bot.colour,
@@ -138,7 +239,6 @@ class Trivia(commands.Cog):
                                            timestamp=datetime.utcnow()
                                            )
                        )
-
 
 
 def setup(bot):
