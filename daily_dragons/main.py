fbf0d5916148d6f399cb218aeac4a706a64b83ae
Valiant55 <matthew.g.stoltz@gmail.com>
2019-07-05 14:18:21 -0400

    removed gui option

--- daily_dragons/main.py
+++ daily_dragons/main.py
@@ -1,23 +1,7 @@
-import pyglet
-from game_modules.start_menu import make_gui
 from game_modules.game import Game
-
-
-def run_gui() -> None:
-    pyglet.resource.path = ["resources"]
-    pyglet.resource.reindex()
-
-    start_window = pyglet.window.Window()
-
-    gui = make_gui(start_window)
-
-    pyglet.app.run()
-
+from colorama import Fore
 
 if __name__ == "__main__":
-    player_name = input("What is your name? ")
-    if player_name == "gui":
-        run_gui()
-    else:
-        game = Game(player_name)
-        game.main()
+    player_name = input(Fore.RED + "What is your name? " + Fore.WHITE)
+    game = Game(player_name)
+    game.main()
