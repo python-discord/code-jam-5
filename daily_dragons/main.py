b1984be880612600abc8e7631b4d5d54dceecec6
AvianAnalyst <aviananalyst@gmail.com>
2019-07-05 14:14:34 -0400

    Complete removal of gui implementations

--- daily_dragons/main.py
+++ daily_dragons/main.py
@@ -1,23 +1,7 @@
-import pyglet
-from game_modules.start_menu import make_gui
 from game_modules.game import Game
-
-
-def run_gui() -> None:
-    pyglet.resource.path = ["resources"]
-    pyglet.resource.reindex()
-
-    start_window = pyglet.window.Window()
-
-    gui = make_gui(start_window)
-
-    pyglet.app.run()
 
 
 if __name__ == "__main__":
     player_name = input("What is your name? ")
-    if player_name == "gui":
-        run_gui()
-    else:
-        game = Game(player_name)
-        game.main()
+    game = Game(player_name)
+    game.main()
