e7976408f8f6ef90abe4ffe1b6899ea26d2c5417
Valiant55 <matthew.g.stoltz@gmail.com>
2019-07-05 15:49:20 -0400

    resolved issue w/ displaying current player and earth stats

--- daily_dragons/game_modules/game.py
+++ daily_dragons/game_modules/game.py
@@ -22,16 +22,6 @@ class Game:
         )
         self.help_menu = "\n".join(help_options)
 
-        self.player_stats_msg = f"Your current stats:\n{self.player}"
-
-        self.planet_stats_msg = "\n".join(
-            [
-                "Earth's current stats:",
-                f"{self.earth.health_summary()}",
-                f"{self.earth}",
-            ]
-        )
-
         self.error_msg = Fore.RED + "Unrecognized input, try again or type help"
 
         self.exit_msg = Fore.YELLOW + "Have a good day, thanks for playing!"
@@ -39,13 +29,6 @@ class Game:
         self.successful_order_msg = Fore.GREEN + "Ok, we've sent that in!"
 
         self.cancelled_order_msg = Fore.RED + "Ok, we'll cancel that order."
-
-        self.input_options = {
-            "help": self.help_menu,
-            "exit": self.exit_msg,
-            "stats": self.player_stats_msg,
-            "earth": self.planet_stats_msg,
-        }
 
         print(
             f"Hello {name},",
@@ -67,6 +50,21 @@ class Game:
             "Good luck!",
             sep="\n",
         )
+
+    def planet_stats_msg(self) -> str:
+        planet_stats_msg = "\n".join(
+            [
+                "Earth's current stats:",
+                f"{self.earth.health_summary()}",
+                f"{self.earth}",
+            ]
+        )
+
+        return planet_stats_msg
+
+    def player_stats_msg(self) -> str:
+        player_stats_msg = f"Your current stats:\n{self.player}"
+        return player_stats_msg
 
     def main(self) -> None:
         while not self.quit_game:
@@ -99,13 +97,21 @@ class Game:
             return self.error_msg
 
     def parse_input(self, token: str) -> Dict.values:
+
+        input_options = {
+            "help": self.help_menu,
+            "exit": self.exit_msg,
+            "stats": self.player_stats_msg(),
+            "earth": self.planet_stats_msg(),
+        }
+
         if token.isdigit():
             return self.investments.options.get(token, self.error_msg)
         elif token.casefold() == "exit":
             self.quit_game = True
-            return self.input_options[token]
+            return input_options[token]
         elif len(token.split(" ")) < 2:
-            return self.input_options.get(token, self.error_msg)
+            return input_options.get(token, self.error_msg)
         else:
             args = token.split(" ")
 
