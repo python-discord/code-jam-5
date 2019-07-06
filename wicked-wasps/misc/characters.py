51284538cae9060cdfec660501b4b1bac2cda1f0
HypoT <hypoturtle@gmail.com>
2019-07-06 16:09:54 +0100

    touchups

--- wicked-wasps/misc/characters.py
+++ wicked-wasps/misc/characters.py
@@ -1,9 +1,10 @@
 class Characters:
     class Player:
-        def __init__(self, member):
+        def __init__(self, member, position):  # , *args, **kwargs):
             self.name = str(member)
             self.member = member
-            self.position = None
+            self.position = position
+            self.is_active = False
 
         def __str__(self):
             return type(self).__name__
@@ -12,13 +13,13 @@ class Characters:
             pass
 
     class polluter(Player):
-        def __init__(self, member):
-            super().__init__(member)
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args)
 
     class Zilla(polluter):
-        def __init__(self, member):
-            super().__init__(member)
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args)
 
     class green_activist(Player):
-        def __init__(self, member):
-            super().__init__(member)
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args)
--- wicked-wasps/misc/table.py
+++ wicked-wasps/misc/table.py
@@ -2,33 +2,39 @@ import math
 import random
 from PIL import Image, ImageDraw
 from characters import Characters as char
+from collections import deque
 
 
 class GameBoard:
-    def __init__(self, members, size=200):
-        self.players = self._players(members)
+    def __init__(self, members, size=300):
+        self.operators = len(members)
         self.size = size
-        self.board_coords()
+        self.apices = self.board_coords()
+        self.inner = self.board_coords(size*.1)
         self.seats = self._seats()
-        self.board = self._board
-        self.seated_players = dict(zip(self.seats, self.players))
+        self.players = deque(self._players(members))
+        self.eliminated = []
+        self.board = self._board()
+        self.turn_mark = self._turn_mark
 
     def _players(self, members):
         num = len(members)
         acts = num//2+1
         random.shuffle(members)
         players = []
-        for i, j in zip(members,
-                        [char.Zilla] + [char.polluter]*(num-acts-1) + [char.green_activist]*acts):
-            players.append(j(i))
+        for faction, player, position in zip(
+            [char.Zilla] + [char.polluter]*(num-acts-1) + [char.green_activist]*acts,
+            members, self.seats
+        ):
+            players.append(faction(player, position))
         random.shuffle(players)
         return players
 
-    def board_coords(self):
-        sides = len(self.players)
+    def board_coords(self, border=20):
+        sides = self.operators
         center = [self.size/2] * 2
         segment = math.pi * 2 / sides
-        radius = self.size / 2 - 20
+        radius = self.size / 2 - border
         rotation = segment - math.pi
         if sides % 2 == 0:
             rotation /= 2
@@ -36,7 +42,7 @@ class GameBoard:
                    math.cos(segment * side + rotation) * radius) for side in range(sides)]
         if center:
             points = [tuple(sum(pair) for pair in zip(point, center)) for point in points]
-        self.apices = points
+        return points
 
     def _seats(self):
         pos = self.apices + [self.apices[0]]
@@ -45,11 +51,20 @@ class GameBoard:
 
     def _board(self):
         board = Image.new('RGBA', (self.size, self.size), (50, 50, 50, 0))
-        draw = ImageDraw.ImageDraw(Board)
+        draw = ImageDraw.ImageDraw(board, 'RGBA')
         draw.polygon(self.apices, fill=(200, 200, 200))
-        for i, j in self.seated_players.items():
-            draw.text(i, j.name, fill=(125, 125, 25))
-        board.show()
+        draw.polygon(self.inner, fill=(78, 46, 40))
+        for player in self.players:
+            draw.text(player.position, player.name, fill=(125, 125, 25))
+        return board
 
-    def turn_marker(self):
-        pass
+    def _turn_mark(self):
+        running_bd = self.board.copy()
+        draw = ImageDraw.ImageDraw(running_bd, 'RGBA')
+        active = self.players[0]
+        active.active = True
+        x, y = active.position
+        r = self.size*.1
+        draw.ellipse((x-r, y-r, x+r, y+r), outline=(255, 223, 0, 0))
+        self.players.rotate(1)
+        return running_bd
