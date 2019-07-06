ae682181e01536a8667e7c5f7b04626f6c1eba9b
0xf0f <0x0meta@gmail.com>
2019-07-07 11:37:03 +1200

    fixed a linting issue, added docs

--- thoughtful_termites/app/qt.py
+++ thoughtful_termites/app/qt.py
@@ -1,2 +1,12 @@
-from PyQt5.Qt import *
+"""
+This module is used a central access point for all Qt classes and values.
+"""
+
+import PyQt5.Qt as QtBase
+from PyQt5.Qt import *  # noqa
 from PyQt5.Qt import Qt as constants
+
+__all__ = [
+    'constants',
+    *dir(QtBase),
+]
