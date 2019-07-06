3cecddfe472ae578b16f526f070b98d6ae0eaac4
Pip-ayy <pipay@hotmail.co.uk>
2019-07-06 13:19:12 +0100

    basic world changes in place, barebones virus changes

--- mysterious_moose/src/turn_simulation.py
+++ mysterious_moose/src/turn_simulation.py
@@ -34,13 +34,43 @@ class WeatherEvent:
             World.co2_concentation += 0.5
 
 
+def simulate_world_changes(world, virus):
+    # calculate sea level rises
+    initial_sea_level = world.sea_level
+    sea_level_rise = (world.co2_concentration - 410) * 0.02
+    world.sea_level += sea_level_rise
+
+    for region in world.regions:
+        # assume population is evenly distributed between 1m and average elevation
+        region.population -= math.ceil((world.sea_level - initial_sea_level - 1) / region.population)
+        if region.population <= 0:
+            region.population = 0
+            region.destroyed = 1
+
+    world.temperature_rise += (world.co2_concentration - 410) * 0.05
+
+    for region in virus.affected_regions:
+        world.co2_concentration += virus.impact
+
+    return world
+
+
+def simulate_virus_changes(virus):
+    pass
+
+def simulate_turn(world, virus):
+    world = simulate_world_changes(world, virus)
+    virus = simulate_virus_changes(virus)
+    return [world, virus]
+
+
 # model extreme weather events
-
-number_of_events = math.floor(random.randint(0, 21 + (earth.sea_level * earth.co2_concentration * 0.01)) / 20)
-
-for i in range(number_of_events):
-    event = WeatherEvent(random.choice(weather_event_types))
-    earth.region[event.region].population -= event.death_toll
-    if earth.region[event.region].population <= 0:
-        earth.region[event.region].population = 0
-        earth.region[event.region].destroyed = 1
+# def simulate_weather_events():
+#
+#    number_of_events = math.floor(random.randint(0, 21 + (earth.sea_level * earth.co2_concentration * 0.01)) / 20)
+#    for i in range(number_of_events):
+#        event = WeatherEvent(random.choice(weather_event_types))
+#        earth.region[event.region].population -= event.death_toll
+#        if earth.region[event.region].population <= 0:
+#            earth.region[event.region].population = 0
+#            earth.region[event.region].destroyed = 1
