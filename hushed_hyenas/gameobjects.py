7d00e6d4ff06fa6144c7a6a195d365c7256393d9
Felipe <ofelipedbzgt@hotmail.com>
2019-07-06 00:23:19 -0300

    Implemented example treeview widget with tkinter

--- hushed_hyenas/gameobjects.py
+++ hushed_hyenas/gameobjects.py
@@ -7,12 +7,32 @@ from time import sleep
 import pygame
 import thorpy
 from tkinter import *
+from tkinter import ttk
 
 
 def upgrade_menu():
     upgrade_window = Tk()
     upgrade_window.geometry('400x400+0+0')
     upgrade_window.title("Upgrades window")
+
+    # Example tkinter treeview widget use
+    tree = ttk.Treeview(upgrade_window)
+    tree["columns"] = ("one", "two")
+    tree.column("one", width=150)
+    tree.column("two", width=100)
+    tree.heading("one", text="column A")
+    tree.heading("two", text="column B")
+    tree.insert("", 0, text="Line 1", values=("1A", "1b"))
+    tree.insert("", "end", text="sub dir 2", values=("2A", "2B"))
+    # insert sub-item, method 1
+    id2 = tree.insert("", "end", "dir2", text="Dir 2")
+    tree.insert(id2, "end", text="sub dir 2-1", values=("2A", "2B"))
+    tree.insert(id2, "end", text="sub dir 2-2", values=("2A-2", "2B-2"))
+    # insert sub-item, method 2
+    tree.insert("", "end", "dir3", text="Dir 3")
+    tree.insert("dir3", "end", text=" sub dir 3", values=("3A", "3B"))
+
+    tree.pack()
 
     upgrade_window.mainloop()
 
