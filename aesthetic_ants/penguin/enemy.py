11e753b175f2fac218b71c57f91b7abb27da029e
Bast0006 <bast@fastmail.com>
2019-07-06 23:15:20 -0700

    Make enemies bounce off walls

--- aesthetic_ants/penguin/enemy.py
+++ aesthetic_ants/penguin/enemy.py
@@ -1,7 +1,7 @@
 import random
 
 from .snowball import Snowball
-from .constants import CollisionType
+from .constants import CollisionType, TileType
 from .object import PhysicalObject
 from .resources import ENEMY_IMAGE
 
@@ -23,3 +23,20 @@ class Enemy(PhysicalObject):
     def on_collision_snowball(self, snowball: Snowball):
         self.space.remove(self)
         self.space.remove(snowball)
+
+    def collide_tile(self, tile):
+        if tile.tile_type == TileType.WALL:
+            tile_offset_x = abs(self.x - tile.x)
+            tile_offset_y = abs(self.y - tile.y)
+
+            if tile_offset_y < tile_offset_x < self.width * self.collision_leniency // 2:
+                if tile.x > self.x and self.velocity_x > 0:
+                    self.velocity_x *= -1
+                if tile.x < self.x and self.velocity_x < 0:
+                    self.velocity_x *= -1
+
+            if tile_offset_x < tile_offset_y < self.height * self.collision_leniency // 2:
+                if tile.y > self.y and self.velocity_y > 0:
+                    self.velocity_y *= -1
+                if tile.y < self.y and self.velocity_y < 0:
+                    self.velocity_y *= -1
