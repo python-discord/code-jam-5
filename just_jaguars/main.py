bdc0920fe6747d8e6fc0ea50d84ecf0df4704153
A5rocks <40616000+A5rocks@users.noreply.github.com>
2019-07-07 17:40:39 -0400

    tutorial

--- just_jaguars/main.py
+++ just_jaguars/main.py
@@ -365,6 +365,39 @@ def fps_counter():
         fps_text = arial.render(f'FPS: {fps_displayed}/60', False, (0, 0, 0))
     window.blit(fps_text, (0, window_height - 20))
 
+def tutorial():
+    """Credit goes to https://docs.opencv.org/master/dd/d43/tutorial_py_video_display.html"""
+    import cv2
+
+    cap = cv2.VideoCapture('assets/tutorial.mov')
+
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+
+        if not ret or cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        cv2.imshow('frame', frame)
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+# test if the tutorial needs to be played
+
+try:
+    tutorial_file = open('saves/tutorial.txt')  # error causing function 1
+
+    tutorial_content = tutorial_file.read()
+    tutorial_file.close()
+    del tutorial_file
+
+    assert tutorial_content == 'done'  # error causing function 2
+
+except:
+    tutorial()
+    with open('saves/tutorial.txt', 'w') as f:
+        f.write('done')
+
 
 pause = False
 running = True
