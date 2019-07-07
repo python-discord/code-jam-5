5087022a70b9bd99c77b3ea655c22d4b481e0f6d
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2019-07-07 20:26:40 +0300

    documenting credits and main menu

--- various_vipers/project/UI/page/credits.py
+++ various_vipers/project/UI/page/credits.py
@@ -29,15 +29,20 @@ class Credits:
 
     def __init__(self, screen: pg.Surface):
         self.screen = screen
+
+        # images of the back button and its hover state
         back_btn_img = load_img(BTN["back-btn"])
         back_btn_img_h = load_img(BTN["back-btn-hover"])
 
+        # layout of the credits and background image
         self.credits = load_img(PATH_CREDITS)
         self.background = load_img(PATH_CREDITS_BG)
 
+        # rectangles for infinity looping the backgroud image
         self.bg_rect_1 = pg.Rect(0, 0, WIDTH, HEIGHT)
         self.bg_rect_2 = pg.Rect(-WIDTH, 0, WIDTH, HEIGHT)
 
+        # create an instance of a back button
         self.back_btn = Button(
             screen=self.screen,
             x=ButtonProperties.back_btn_x,
@@ -48,29 +53,47 @@ class Credits:
             image_hover=back_btn_img_h,
         )
 
-    def draw(self, mouse_x: int, mouse_y: int, event):
+    def draw(self, mouse_x: int, mouse_y: int, event) -> None:
         """Hadle all options events and draw elements."""
+        # draw the infinity background and credits layout
         self.__draw_infinity_bg()
         self.screen.blit(self.credits, (0, 0, WIDTH, HEIGHT))
 
+        # check if back buttn is hovered
         if self.back_btn.rect.collidepoint(mouse_x, mouse_y):
+            # draw its hover state
             self.back_btn.draw(hover=True)
 
+            # if the back button is clicked play click sound and
+            # return to the main menu
             if event.type == pg.MOUSEBUTTONDOWN:
                 Sound.click.play()
                 return WindowState.main_menu
         else:
+            # if it is not hover draw its normal state
             self.back_btn.draw()
         return WindowState.credit
 
-    def __draw_infinity_bg(self):
+    def __draw_infinity_bg(self) -> None:
+        """
+        Draws the infinity backround.
+
+        It uses two rectangles to swap the images.
+        The two rectangles are moving in one direction.
+
+        One of them is always with WIDTH ahead of the other rectangle.
+        So if it reaches the end, every rectangle goes back with -WIDTH.
+        """
+        # increase their position on the x axis with one pixel
         self.bg_rect_1.left += 1
         self.bg_rect_2.left += 1
 
+        # if the rects reach the end they go with -WIDTH back
         if self.bg_rect_1.left == WIDTH:
             self.bg_rect_1.left = -WIDTH
         if self.bg_rect_2.left == WIDTH:
             self.bg_rect_2.left = -WIDTH
 
+        # draw them on the screen.
         self.screen.blit(self.background, self.bg_rect_1)
         self.screen.blit(self.background, self.bg_rect_2)
--- various_vipers/project/UI/page/main_menu.py
+++ various_vipers/project/UI/page/main_menu.py
@@ -39,16 +39,20 @@ class MainMenu:
         """Set initial main menu values."""
         self.screen = screen
 
+        # load the main menu button images
         self.__load_images()
 
+        # create buttons, store them and its states and extra github button
         self.__create_buttons()
-        self.__store_buttons_and_states()
+        self.__store_buttons_and_states()  # the states are what the buttons do
         self.__load_set_github_button()
 
+        # use last click timer for preventing too much clicking on the button
         self.last_click = int()
         self.font = Font(None, 40)
 
-    def __load_images(self):
+    def __load_images(self) -> None:
+        """Loads all main menu button images and their hover states."""
         img_paths = [
             (BTN["play-btn"], BTN["play-btn-hover"]),
             (BTN["options-btn"], BTN["options-btn-hover"]),
@@ -62,27 +66,40 @@ class MainMenu:
 
     def draw(self, mouse_x: int, mouse_y: int, event) -> str:
         """Hadles all main menu events and draw every elements."""
+        # assign the events with self for easy usage in other methods
         self.event = event
         self.mouse_x, self.mouse_y = mouse_x, mouse_y
 
         self.clicked = event.type == pg.MOUSEBUTTONDOWN
-        # hover check for the play button
+        # iterate all main menu buttons except the github icon
         for i, button in enumerate(self.buttons):
+            # check if are hovered
             if button.rect.collidepoint(mouse_x, mouse_y):
-
+                # draw button hover state
                 button.draw(hover=True)
 
                 if self.clicked:
+                    # if the button is clicked - play the sound and
+                    # return the state (the action) which the button does
                     Sound.click.play()
                     return self.states[i]
             else:
+                # else draw its normal state
                 button.draw()
+
+        # draw the github button icon linking to our repo (bottom left)
+        # and draw the highscore (bottom right)
         self.__draw_github_button()
         self.__draw_highscores()
 
         return WindowState.main_menu
 
-    def __create_buttons(self):
+    def __create_buttons(self) -> None:
+        """
+        Create main menu buttons on particular layout.
+
+        Every main menu buttons centered and alligned with margin y on top and botoom
+        """
         self.play_btn, self.opt_btn, self.credits_btn, self.quit_btn = generate_main_buttons(
             screen=self.screen,
             btn_w=ButtonProperties.main_btn_w,
@@ -92,7 +109,17 @@ class MainMenu:
             images=self.images,
         )
 
-    def __store_buttons_and_states(self):
+    def __store_buttons_and_states(self) -> None:
+        """
+        Create two lists for buttons and their acctions.
+
+        So every button index coresponds to its action.
+
+        The play button starts the game.
+        The options button opens options page.
+        The credits button opens credits page.
+        The quit button quits the game.
+        """
         self.buttons = [self.play_btn, self.opt_btn, self.credits_btn, self.quit_btn]
         self.states = [
             WindowState.game,
@@ -101,7 +128,12 @@ class MainMenu:
             WindowState.quited,
         ]
 
-    def __load_set_github_button(self):
+    def __load_set_github_button(self) -> None:
+        """
+        Create the github icon button on the bottom right corner.
+
+        This button links to our repository.
+        """
         image = load_img(BTN["github"])
         image_hover = load_img(BTN["github-hover"])
 
@@ -115,7 +147,12 @@ class MainMenu:
             image_hover=image_hover,
         )
 
-    def __draw_github_button(self):
+    def __draw_github_button(self) -> None:
+        """
+        Draws the github icon button and its hover state.
+
+        If it is clicked, it opens our repository in GitHub.
+        """
         if self.github_btn.rect.collidepoint(self.mouse_x, self.mouse_y):
             self.github_btn.draw(hover=True)
             if self.clicked and (time.time() - self.last_click) > 0.3:
@@ -125,7 +162,8 @@ class MainMenu:
         else:
             self.github_btn.draw()
 
-    def __draw_highscores(self):
+    def __draw_highscores(self) -> None:
+        """Draws highscores in the bottom left."""
         date = realtime_to_ingame_delta_formatted(user_data.hiscore_modern)
         text = self.font.render(f"Highscore: {date}", True, Color.blue)
 
