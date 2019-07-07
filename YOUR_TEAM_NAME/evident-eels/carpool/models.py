de32b54bfef96dbefec6690ee4893fa849763777
fluzz142857 <31717510+fluzz142857@users.noreply.github.com>
2019-07-06 20:18:52 -0700

    Add from and to locations to carpool model

--- YOUR_TEAM_NAME/evident-eels/carpool/models.py
+++ YOUR_TEAM_NAME/evident-eels/carpool/models.py
@@ -14,6 +14,8 @@ class Carpool(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), index=True, unique=True)
     summary = db.Column(db.String(128))
+    from_location = db.Column(db.string(32))
+    to_location = db.Column(db.string(32))
 
     def __repr__(self):
         return "<Carpool: {}>".format(self.name)
@@ -22,10 +24,10 @@ class Carpool(db.Model):
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), index=True, unique=True)
-    password = db.Column(db.String(200), primary_key = False, unique=False, nullable=False)
+    password = db.Column(
+        db.String(200), primary_key=False, unique=False, nullable=False
+    )
     email = db.Column(db.String(40), index=True, unique=True, nullable=False)
-    #created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True)
-    #last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
     carpools = db.relationship(
         "Carpool",
         secondary=passengers,
@@ -35,19 +37,21 @@ class User(UserMixin, db.Model):
         lazy="dynamic",
     )
 
-    def set_password(self,password):
+    def set_password(self, password):
         """Create hashed password."""
-        self.password = generate_password_hash(password, method = 'sha256')
+        self.password = generate_password_hash(password, method="sha256")
 
-    def check_password(self,password):
+    def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
 
     def __repr__(self):
         return "<User: {}>".format(self.name)
 
+
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 db.create_all()
