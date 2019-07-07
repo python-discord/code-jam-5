b64c368dbe48e01b2f1bfe8b80274d4f0c9b3eab
Valiant55 <matthew.g.stoltz@gmail.com>
2019-07-07 17:49:40 -0400

    removed news :(

--- daily_dragons/game_modules/investment.py
+++ daily_dragons/game_modules/investment.py
@@ -1,5 +1,4 @@
 from typing import List
-from .news import News
 from .organization import Organization
 from .policy import Policy
 
@@ -8,18 +7,11 @@ class Investment:
     """
     Investments are choices presented to the player on how they
     allocate their money. Investements will have a positive, negative
-    or possiblely no affect on the planet and could generate news, ROI funds
+    or possiblely no affect on the planet and could generate ROI funds
     or unlock new investment opportunities in future rounds.
     """
 
-    def __init__(
-        self,
-        organization: Organization,
-        policies: List[Policy],
-        news_on_apperance: List[News] = None,
-        news_on_investment: List[News] = None,
-        news_on_no_investment: List[News] = None,
-    ) -> None:
+    def __init__(self, organization: Organization, policies: List[Policy]) -> None:
         self.organization = organization
         self.policies = policies
         self.news_on_apperance = news_on_apperance
