124426b3cbdbc3ab9c74afcb060c177e2b1c9f9f
Valiant55 <matthew.g.stoltz@gmail.com>
2019-07-07 18:07:08 -0400

    added ascii art banner

--- daily_dragons/main.py
+++ daily_dragons/main.py
@@ -1,7 +1,9 @@
 from game_modules.game import Game
 from colorama import Fore
+import pyfiglet
 
 if __name__ == "__main__":
+    print(pyfiglet.figlet_format("Who wants to be a Billionare?"))
     player_name = input(Fore.RED + "What is your name? " + Fore.WHITE)
     game = Game(player_name)
     game.main()
