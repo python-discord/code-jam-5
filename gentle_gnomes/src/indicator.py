dabe35607ce6814b815f56eb20399622b529f68d
MarkKoz <KozlovMark@gmail.com>
2019-07-06 21:03:14 -0700

    Get indicator data for each scenario concurrently

--- gentle_gnomes/src/indicator.py
+++ gentle_gnomes/src/indicator.py
@@ -1,8 +1,8 @@
 import asyncio
 import json
+import typing as t
 from collections import Counter
 from itertools import chain
-from typing import Tuple
 
 import numpy as np
 from quart import current_app as app
@@ -26,16 +26,22 @@ class Indicator:
         self.description = None
         self.units = None
         self.rate = None
-        self.plot = None
         self.x = None
         self.y = None
 
+    async def _get_data(self) -> t.List[t.Dict]:
+        tasks = []
+        for scenario in ('historical', 'RCP85'):
+            tasks.append(app.azavea.get_indicator_data(self.city.id, scenario, self.name))
+
+        return await asyncio.gather(*tasks)
+
     async def populate_data(self):
+        """Populate the indicator with data from the API for the historical and RCP85 scenarios."""
         items = []
         count = 0
 
-        for scenario in ('historical', 'RCP85'):
-            response = await app.azavea.get_indicator_data(self.city.id, scenario, self.name)
+        for response in await self._get_data():
             self.label = response['indicator']['label']
             self.description = response['indicator']['description']
             self.units = response['units']
@@ -64,7 +70,7 @@ async def _create_indicator(name, city):
     return name, indicator
 
 
-async def get_top_indicators(city: City, n: int = 5) -> Tuple[Indicator, ...]:
+async def get_top_indicators(city: City, n: int = 5) -> t.Tuple[Indicator, ...]:
     """Return the top n indicators with the highest rate of change."""
     tasks = [_create_indicator(name, city) for name in INDICATORS]
     indicators = dict(await asyncio.gather(*tasks))
