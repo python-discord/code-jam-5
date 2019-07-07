2f8f3dde1c2d34c59900d3d3e6f861a158b75095
Ikusaba-san <Ikusaba-san@users.noreply.github.com>
2019-07-07 13:25:16 -0700

    Update hardcoded points

--- aesthetic_ants/penguin/game.py
+++ aesthetic_ants/penguin/game.py
@@ -25,7 +25,8 @@ class Game(pyglet.window.Window):
         self.space.add(self.player)
 
         self.spawner = Spawner()
-        self.spawner.add_spawn_point(0, 0)
+        self.spawner.add_spawn_point(64, self.height / 2)
+        self.spawner.add_spawn_point(self.width - 64, self.height / 2)
         self.spawner.wave = Wave.load('resources/waves/1.wave')
         self.space.add(self.spawner)
 
