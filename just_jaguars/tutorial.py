0e2d94ff0f4a21931ecee260a3fd32b37c28fc33
A5rocks <40616000+A5rocks@users.noreply.github.com>
2019-07-07 18:58:55 -0400

    fix tutorial video

--- just_jaguars/tutorial.py
+++ just_jaguars/tutorial.py
@@ -1,22 +1,23 @@
 import cv2
+import time
 
 def tutorial():
     """Credit goes to https://docs.opencv.org/master/dd/d43/tutorial_py_video_display.html"""
     cap = cv2.VideoCapture('assets/tutorial.mov')
 
-    while(cap.isOpened()):
+    while cap.isOpened():
         ret, frame = cap.read()
 
         if not ret or cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
         cv2.imshow('tutorial', frame)
+        time.sleep(1/30) # 30 fps
 
     cap.release()
     cv2.destroyAllWindows()
 
 # test if the tutorial needs to be played
-
 
 try:
     tutorial_file = open('saves/tutorial.txt')  # error causing function 1
