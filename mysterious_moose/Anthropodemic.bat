7f829addca7c47412d0dfc33ff299e8463914a31
Leon Sandøy <leon.sandoy@conmodo.com>
2019-07-13 16:56:21 +0200

    Removing mysterious_moose folder. It doesn't lint, and this will make all other PR's fail their builds.

--- mysterious_moose/Anthropodemic.bat
+++
@@ -1,9 +0,0 @@
-if not exist venv/ (
-    python -m venv venv
-    venv\Scripts\activate.bat
-    python -m pip install -r requirements.txt
-    python -m src
-) else (
-    venv\Scripts\activate.bat
-    python -m src
-)
--- mysterious_moose/tests/__main__.py
+++
@@ -1,7 +0,0 @@
-import tests.blocks as block
-
-
-blocks_file = block.test_file()
-
-if blocks_file is True:
-    block.test_cards(0.1)
